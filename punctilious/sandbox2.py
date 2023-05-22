ba3983048e7e992988eda1c5b275d41c7e209827
david.doret@me.com <david.doret@me.com>
2023-05-22 22:47:06 +0200

    export to txt

--- punctilious/sandbox2.py
+++ punctilious/sandbox2.py
@@ -1,10 +1,12 @@
+s = 'hello'
+s = s + 'world'
+s = s + '♞'
+# s = s.encode('utf8')
+# s = bytes.decode(s, 'utf8')
+# s = bytes.decode(s)
+print(s)
+# my_bytes = s.encode('utf-8')
 
-
-x = {'a': '1', 'A': '2', 'c': '3'}
-
-print(x.get('a'))
-print(x.get('A'))
-
-import repm
-
-print(repm.serif_bold('aPpA'))
+text_file = open('test.txt', 'w', encoding='utf-8')
+n = text_file.write(s)
+text_file.close()
---
+++ theories/export_theories.py
@@ -0,0 +1,5 @@
+import core
+import natural_numbers_1
+
+core.foundation_theory.export_to_text('foundation_theory.txt')
+natural_numbers_1.t1.export_to_text('natural_numbers_1.txt')
