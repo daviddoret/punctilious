58aafe667591804d25f1740eecb60e94f38f67c2
David Doret <david.doret@me.com>
2023-07-02 22:35:07 +0200

    cleanup

--- tests/test_axiom_interpretation.py
+++ tests/test_axiom_interpretation.py
@@ -6,7 +6,7 @@ import random_data
 class TestAxiomInterpretation(TestCase):
     def test_axiom_interpretation(self):
         pu.configuration.echo_default = False
-        pu.configuration.text_format = pu.text_formats.plaintext
+        pu.configuration.text_format = pu.text_formats.unicode
         pu.configuration.echo_default = False
         u = pu.UniverseOfDiscourse()
         content1 = random_data.random_sentence()
@@ -19,8 +19,8 @@ class TestAxiomInterpretation(TestCase):
         o1 = u.o.declare()
         o2 = u.o.declare()
         o3 = u.o.declare()
-        r1 = u.r.declare(1, signal_proposition=True)
-        r2 = u.r.declare(2, signal_proposition=True)
+        r1 = u.r.declare(1, nameset='r', signal_proposition=True)
+        r2 = u.r.declare(2, nameset='r', signal_proposition=True)
         aii1 = t.i.axiom_interpretation.infer_statement(ai1, u.f(r1, o1))
         self.assertTrue(aii1.valid_proposition.is_formula_equivalent_to(u.f(r1, o1)))
         print(aii1.rep_report())
