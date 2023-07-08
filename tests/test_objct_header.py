696ea16dbf04b39cc8cefb299a86346d1fcfbdc5
David Doret <david.doret@me.com>
2023-07-08 11:34:03 +0200

    tests

--- tests/test_objct_header.py
+++ tests/test_objct_header.py
@@ -8,12 +8,18 @@ class TestHeader(TestCase):
         header_reference = random_data.random_word()
         header = pu.Title(header_reference, cat=pu.title_categories.axiom_declaration)
         self.assertEqual(f'axiom {header_reference}', str(header))
-        self.assertEqual(f'axiom {header_reference}', header.rep())
-        self.assertEqual(f'axiom {header_reference}', header.rep_title())
-        self.assertEqual(f'axiom {header_reference}', header.rep_ref())
-        self.assertEqual(f'Axiom {header_reference}', header.rep(cap=True))
-        self.assertEqual(f'Axiom {header_reference}', header.rep_title(cap=True))
-        self.assertEqual(f'Axiom {header_reference}', header.rep_ref(cap=True))
+        self.assertEqual(f'axiom {header_reference}',
+                         header.rep(text_format=pu.text_formats.plaintext))
+        self.assertEqual(f'axiom {header_reference}',
+                         header.rep_title(text_format=pu.text_formats.plaintext))
+        self.assertEqual(f'axiom {header_reference}',
+                         header.rep_ref(text_format=pu.text_formats.plaintext))
+        self.assertEqual(f'Axiom {header_reference}',
+                         header.rep(text_format=pu.text_formats.plaintext, cap=True))
+        self.assertEqual(f'Axiom {header_reference}',
+                         header.rep_title(text_format=pu.text_formats.plaintext, cap=True))
+        self.assertEqual(f'Axiom {header_reference}',
+                         header.rep_ref(text_format=pu.text_formats.plaintext, cap=True))
 
     def test_header_2(self):
         header_reference = random_data.random_word()
@@ -23,9 +29,13 @@ class TestHeader(TestCase):
         self.assertEqual(f'prop. {header_reference}', str(header))
         self.assertEqual(f'prop. {header_reference}',
                          header.rep(text_format=pu.text_formats.plaintext))
-        self.assertEqual(f'proposition {header_reference} - {header_title}', header.rep_title())
-        self.assertEqual(f'proposition {header_reference}', header.rep_ref())
-        self.assertEqual(f'Proposition {header_reference}', header.rep(cap=True))
+        self.assertEqual(f'proposition {header_reference} - {header_title}',
+                         header.rep_title(text_format=pu.text_formats.plaintext))
+        self.assertEqual(f'prop. {header_reference}',
+                         header.rep_ref(text_format=pu.text_formats.plaintext))
+        self.assertEqual(f'Prop. {header_reference}',
+                         header.rep(text_format=pu.text_formats.plaintext, cap=True))
         self.assertEqual(f'Proposition {header_reference} - {header_title}',
-                         header.rep_title(cap=True))
-        self.assertEqual(f'Proposition {header_reference}', header.rep_ref(cap=True))
+                         header.rep_title(text_format=pu.text_formats.plaintext, cap=True))
+        self.assertEqual(f'Prop. {header_reference}',
+                         header.rep_ref(text_format=pu.text_formats.plaintext, cap=True))
