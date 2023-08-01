52a9c8ffc1c4a88b9cf907f77d59cfbc9f6fa06a
David Doret <david.doret@me.com>
2023-08-01 21:14:16 +0200

    tao_2006_the_peano_axioms

--- theories/export_theories.py
+++ theories/export_theories.py
@@ -9,3 +9,15 @@ t1.export_report_to_file(
 t1.export_report_to_file(
     'tao_2006_the_peano_axioms_without_proofs_en_us_unicode.txt', encoding=pu.encodings.unicode,
     output_proofs=False)
+t1.export_report_to_file(
+    'tao_2006_the_peano_axioms_with_proofs_en_us_plaintext.txt', encoding=pu.encodings.plaintext,
+    output_proofs=True)
+t1.export_report_to_file(
+    'tao_2006_the_peano_axioms_without_proofs_en_us_plaintext.txt', encoding=pu.encodings.plaintext,
+    output_proofs=False)
+t1.export_report_to_file(
+    'tao_2006_the_peano_axioms_with_proofs_en_us_latex.tex', encoding=pu.encodings.latex_math,
+    output_proofs=True)
+t1.export_report_to_file(
+    'tao_2006_the_peano_axioms_without_proofs_en_us_latex.tex', encoding=pu.encodings.latex_math,
+    output_proofs=False)
