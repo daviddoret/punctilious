106297da5ac4e3c0274ca19c9766fd7c59364024
David Doret <david.doret@me.com>
2023-08-06 20:28:09 +0200

    proof by contradiction (incomplete)

--- tests/test_inconsistency_introduction.py
+++ tests/test_inconsistency_introduction.py
@@ -18,6 +18,6 @@ class TestInconsistencyIntroduction(TestCase):
         p = t.i.axiom_interpretation.infer_statement(ap, u.f(r1, o1, o2), echo=True)
         not_p = t.i.axiom_interpretation.infer_statement(ap, u.f(u.r.lnot, u.f(r1, o1, o2)),
                                                          echo=True)
-        inc = t.i.inconsistency_introduction.infer_statement(p, not_p, echo=True)
+        inc = t.i.inconsistency_introduction.infer_statement(p=p, not_p=not_p, echo=True)
         self.assertEqual('𝐼𝑛𝑐(𝒯₁)', inc.rep_formula(encoding=pu.encodings.unicode))
         self.assertIs(pu.consistency_values.proved_inconsistent, t.consistency)
--- tests/test_proof_by_contradiction.py
+++ tests/test_proof_by_contradiction.py
@@ -17,28 +17,30 @@ class TestProofByContradiction(TestCase):
         r1 = u.r.declare(2, signal_proposition=True)
         # Elaborate the parent theory
         t1 = u.t()
-        a2 = t1.include_axiom(a=a1)
-        p1 = t1.i.axiom_interpretation.infer_statement(axiom=a2, formula=u.f(r1, o1, o2))
-        p2 = t1.i.axiom_interpretation.infer_statement(axiom=a2, formula=u.f(r1, o2, o3))
-        with u.v() as x, u.v() as y, u.v() as z:
-            p3_implication = t1.i.axiom_interpretation.infer_statement(
-                axiom=a2,
+        t1_a2 = t1.include_axiom(a=a1)
+        t1_p1 = t1.i.axiom_interpretation.infer_statement(axiom=t1_a2, formula=u.f(r1, o1, o2))
+        t1_p2 = t1.i.axiom_interpretation.infer_statement(axiom=t1_a2, formula=u.f(r1, o2, o3))
+        with u.v() as t2_p5, u.v() as y, u.v() as z:
+            t1_p3_implication = t1.i.axiom_interpretation.infer_statement(
+                axiom=t1_a2,
                 formula=u.f(u.r.implies,
-                            u.f(u.r.land, u.f(r1, x, y),
+                            u.f(u.r.land, u.f(r1, t2_p5, y),
                                 u.f(r1, y, z)),
-                            u.f(r1, x, z)))
+                            u.f(r1, t2_p5, z)))
         t1.stabilize()
         hypothetical_formula = u.f(u.r.lnot, u.f(r1, o1, o3))
         # H1: ¬(𝑟₁(𝑜₁, 𝑜₃))
-        hypothesis = t1.pose_hypothesis(hypothetical_proposition=hypothetical_formula)
+        t1_h1 = t1.pose_hypothesis(hypothetical_proposition=hypothetical_formula)
         # TODO: The hypothetical-theory must be stabilized immediately,
         #   otherwise new axioms or definitions may be introduced,
         #   leading to inconsistent results from the perspective of the
         #   base theory.
-        hypothetical_theory = hypothesis.hypothetical_theory
-        p5 = hypothetical_theory.i.conjunction_introduction.infer_statement(p=p1, q=p2)
-        p6 = hypothetical_theory.i.variable_substitution.infer_statement(p3_implication, o1, o2, o3)
+        t2 = t1_h1.hypothetical_theory
+        t2_p5 = t1_h1.hypothetical_proposition
+        t2_p6 = t2.i.conjunction_introduction.infer_statement(p=t1_p1, q=t1_p2)
+        t2_p7 = t2.i.variable_substitution.infer_statement(t1_p3_implication, o1, o2, o3)
         # p7: 𝑟₁(𝑜₁, 𝑜₃) by modus ponens
-        p7 = hypothetical_theory.i.modus_ponens.infer_statement(p_implies_q=p6, p=p5)
+        t2_p8 = t2.i.modus_ponens.infer_statement(p_implies_q=t2_p7, p=t2_p6)
         # p7 is in contradiction with the hypothetical_formula
-        # hypothetical_theory.i.inconsistency_introduction(!!!!!)
+        p9 = t1.i.inconsistency_introduction.infer_statement(p=t2_p8, not_p=t2_p5,
+                                                             inconsistent_theory=t2)
