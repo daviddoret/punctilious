e90cdc47557d7bf74395b86f8a8d5bd344cdb995
David Doret <david.doret@me.com>
2023-09-12 00:48:57 +0200

    bump version 1.0.4 -> 1.0.5

--- src/sample/__init__.py
+++ src/sample/__init__.py
@@ -1,2 +1,2 @@
 """This python module contains sample python scripts that illustrate *punctilious* usage."""
-__version__ = "1.0.4"
+__version__ = "1.0.5"
--- src/theory/__init__.py
+++ src/theory/__init__.py
@@ -1,4 +1,4 @@
 """This python module contains mathematical theories formalized with *punctilious*."""
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 from theory.theory_tao_2006_the_peano_axioms import Tao2006ThePeanoAxioms
