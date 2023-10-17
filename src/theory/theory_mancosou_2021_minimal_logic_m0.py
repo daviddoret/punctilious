7c731be5bfec7371731acd3aa803f969a7226ce2
David Doret <david.doret@me.com>
2023-10-18 00:05:10 +0200

    issue #226: develop minimal logic (M0). lock mechanism for axiom-inclusion and axiom-interpretation.

--- src/theory/theory_mancosou_2021_minimal_logic_m0.py
+++ src/theory/theory_mancosou_2021_minimal_logic_m0.py
@@ -23,12 +23,54 @@ class Mancosou2021MinimalLogicM0(pu.Package):
         t = self.t
         section_1 = t.open_section(section_title='Minimal Logic', section_number=1)
 
-        self.pl1 = u.declare_axiom(ref='PL1', symbol=axiom_symbol, index=1,
+        # Axiom: PL1
+        self.pl1_declaration = u.declare_axiom(symbol=axiom_symbol, index=1,
             natural_language=f'𝐴 ⊃ (𝐴 ∧ 𝐴)')
-        inclusion = t.include_axiom(ref='PL1', symbol=axiom_symbol, index=1, a=self.pl1)
+        self.pl1_inclusion = t.include_axiom(ref='PL1', symbol=axiom_symbol, index=1,
+            a=self.pl1_declaration)
         with u.v(symbol='A', auto_index=False) as a:
-            t.i.axiom_interpretation.infer_formula_statement(a=inclusion,
-                p=a | u.r.implies | (a | u.r.land | a))  # inclusion.lock_axiom()
+            t.i.axiom_interpretation.infer_formula_statement(a=self.pl1_inclusion,
+                p=a | u.r.implies | (a | u.r.land | a), lock=True)
+
+        # Axiom: PL2
+        self.pl2_declaration = u.declare_axiom(symbol=axiom_symbol, index=2,
+            natural_language=f'(𝐴 ∧ 𝐵) ⊃ (𝐵 ∧ 𝐴)')
+        self.pl2_inclusion = t.include_axiom(ref='PL2', symbol=axiom_symbol, index=2,
+            a=self.pl2_declaration)
+        with u.v(symbol='A', auto_index=False) as a, u.v(symbol='B', auto_index=False) as b:
+            t.i.axiom_interpretation.infer_formula_statement(a=self.pl2_inclusion,
+                p=(a | u.r.land | b) | u.r.implies | (b | u.r.land | a), lock=True)
+
+        # Axiom: PL3
+        self.pl3_declaration = u.declare_axiom(symbol=axiom_symbol, index=3,
+            natural_language=f'(𝐴 ⊃ 𝐵) ⊃ [(𝐴 ∧ 𝐶) ⊃ (𝐵 ∧ 𝐶)]')
+        self.pl3_inclusion = t.include_axiom(ref='PL3', symbol=axiom_symbol, index=3,
+            a=self.pl3_declaration)
+        with u.v(symbol='A', auto_index=False) as a, u.v(symbol='B', auto_index=False) as b, u.v(
+                symbol='C', auto_index=False) as c:
+            t.i.axiom_interpretation.infer_formula_statement(a=self.pl3_inclusion,
+                p=(a | u.r.implies | b) | u.r.implies | (
+                        (a | u.r.land | c) | u.r.implies | (b | u.r.land | c)), lock=True)
+
+        # Axiom: PL4
+        self.pl4_declaration = u.declare_axiom(symbol=axiom_symbol, index=4,
+            natural_language=f'[(𝐴 ⊃ 𝐵) ∧ (𝐵 ⊃ 𝐶)] ⊃ (𝐴 ⊃ 𝐶)')
+        self.pl4_inclusion = t.include_axiom(ref='PL4', symbol=axiom_symbol, index=4,
+            a=self.pl4_declaration)
+        with u.v(symbol='A', auto_index=False) as a, u.v(symbol='B', auto_index=False) as b, u.v(
+                symbol='C', auto_index=False) as c:
+            t.i.axiom_interpretation.infer_formula_statement(a=self.pl4_inclusion,
+                p=((a | u.r.land | c) | u.r.implies | (b | u.r.land | c)) | u.r.implies | (
+                        a | u.r.implies | b), lock=True)
+
+        # Axiom: PL5
+        self.pl5_declaration = u.declare_axiom(symbol=axiom_symbol, index=5,
+            natural_language=f'𝐵 ⊃ (𝐴 ⊃ 𝐵)')
+        self.pl5_inclusion = t.include_axiom(ref='PL5', symbol=axiom_symbol, index=5,
+            a=self.pl5_declaration)
+        with u.v(symbol='A', auto_index=False) as a, u.v(symbol='B', auto_index=False) as b:
+            t.i.axiom_interpretation.infer_formula_statement(a=self.pl5_inclusion,
+                p=b | u.r.implies | (a | u.r.implies | b), lock=True)
 
 
 test = Mancosou2021MinimalLogicM0()
