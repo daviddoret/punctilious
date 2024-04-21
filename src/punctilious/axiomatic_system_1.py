b001857d28bbbc57bb829f9e94261f9fa21a672f
daviddoret <david.doret@me.com>
2024-04-21 19:32:02 +0200

    version 2.0: re-starting from scratch with a new data model.

--- src/punctilious/axiomatic_system_1.py
+++ src/punctilious/axiomatic_system_1.py
@@ -32,10 +32,15 @@ class Connective:
         return FormulaBuilder(c=self)
 
 
-class TermsBuilder(list):
-    """A mutable collection of formula terms."""
+class FormulaBuilder(list):
+    """A mutable object to edit and elaborate formulas.
+    Note: formula-builder may be syntactically inconsistent."""
 
-    def __init__(self, terms: FlexibleTerms = None):
+    def __init__(self, c: typing.Optional[Connective] = None, terms: FlexibleElements = None):
+        """
+        :param FlexibleTerms terms: A collection of terms."""
+        self.c: typing.Optional[Connective] = c
+
         # When inheriting from list, we implement __init__ and not __new__.
         # Reference: https://stackoverflow.com/questions/9432719/python-how-can-i-inherit-from-the-built-in-list-type
         super().__init__(self)
@@ -52,9 +57,11 @@ class TermsBuilder(list):
     def __str__(self):
         return self.rep()
 
-    def append(self, term: FlexibleFormula):
+    def append(self, term: FlexibleFormula) -> FormulaBuilder:
+        """Append a new term to the formula."""
         term = coerce_formula_builder(phi=term)
         super().append(term)
+        return term
 
     def assure_term(self, i: int) -> None:
         """Assure the presence of an i-th term (i being the 0-based index).
@@ -62,9 +69,21 @@ class TermsBuilder(list):
         while len(self) <= i:
             self.append(FormulaBuilder())
 
-    def rep(self, **kwargs) -> str:
+    def iterate_canonical(self):
+        """A top-down, left-to-right iteration."""
+        yield self
+        for t in self:
+            yield from t.iterate_canonical()
+
+    def rep(self, **kwargs):
+        parenthesis = kwargs.get('parenthesis', False)
         kwargs['parenthesis'] = True
-        return ', '.join(term.rep(**kwargs) for term in self)
+        if len(self.terms) == 0:
+            return self.c.rep(**kwargs)
+        if len(self.terms) == 2:
+            return f'{"(" if parenthesis else ""}{self.term_0.rep(**kwargs)} {self.c.rep(**kwargs)} {self.term_1.rep(**kwargs)}{")" if parenthesis else ""}'
+        else:
+            return f'{"(" if parenthesis else ""}{self.c.rep(**kwargs)}({self.terms.rep(**kwargs)}){")" if parenthesis else ""}'
 
     @property
     def term_0(self) -> FormulaBuilder:
@@ -94,73 +113,8 @@ class TermsBuilder(list):
         else:
             self[1] = term
 
-    def to_terms(self) -> Terms:
-        """Return an immutable terms collection that is equivalent to this term-builder."""
-        terms: tuple[Formula, ...] = tuple(coerce_formula(phi=term) for term in self)
-        terms: Terms = Terms(terms=terms)
-        return terms
-
-
-class FormulaBuilder:
-    """A mutable object to edit and elaborate formulas.
-    Note: formula-builder may be syntactically inconsistent."""
-
-    def __init__(self, c: typing.Optional[Connective] = None, terms: FlexibleTerms = None):
-        """
-        :param FlexibleTerms terms: A collection of terms."""
-        self.c: typing.Optional[Connective] = c
-        self.terms: TermsBuilder = coerce_terms_builder(terms=terms)
-
-    def __repr__(self):
-        return self.rep()
-
-    def __str__(self):
-        return self.rep()
-
-    def append_term(self, c: typing.Optional[Connective]) -> FormulaBuilder:
-        t = FormulaBuilder(c=c)
-        self.terms.append(t)
-        return t
-
-    def assure_term(self, i: int) -> None:
-        self.terms.assure_term(i=i)
-
-    def iterate_canonical(self):
-        """A top-down, left-to-right iteration."""
-        yield self
-        for t in self.terms:
-            yield from t.iterate_canonical()
-
-    def rep(self, **kwargs):
-        parenthesis = kwargs.get('parenthesis', False)
-        kwargs['parenthesis'] = True
-        if len(self.terms) == 0:
-            return self.c.rep(**kwargs)
-        if len(self.terms) == 2:
-            return f'{"(" if parenthesis else ""}{self.term_0.rep(**kwargs)} {self.c.rep(**kwargs)} {self.term_1.rep(**kwargs)}{")" if parenthesis else ""}'
-        else:
-            return f'{"(" if parenthesis else ""}{self.c.rep(**kwargs)}({self.terms.rep(**kwargs)}){")" if parenthesis else ""}'
-
-    @property
-    def term_0(self) -> FormulaBuilder:
-        """A shortcut for self.terms.term_0"""
-        return self.terms.term_0
-
-    @term_0.setter
-    def term_0(self, term: FormulaBuilder) -> None:
-        self.terms.term_0 = term
-
-    @property
-    def term_1(self) -> FormulaBuilder:
-        """A shortcut for self.terms.term_1"""
-        return self.terms.term_1
-
-    @term_1.setter
-    def term_1(self, term: FormulaBuilder) -> None:
-        self.terms.term_1 = term
-
     def to_formula(self) -> Formula:
-        terms: Terms = self.terms.to_terms()
+        terms: tuple[Formula, ...] = tuple(coerce_formula(phi=term) for term in self)
         phi: Formula = Formula(c=self.c, terms=terms)
         return phi
 
@@ -171,19 +125,24 @@ class FormulaBuilder:
         return True
 
 
-class Terms(tuple):
-    """An immutable collection of formula terms."""
+class Formula(tuple):
+    """An immutable formula modeled as an edge-ordered, node-colored tree."""
 
-    def __new__(cls, terms: FlexibleTerms = None):
+    def __new__(cls, c: Connective, terms: FlexibleElements = None):
         # When we inherit from tuple, we must implement __new__ instead of __init__ to manipulate arguments,
         # because tuple is immutable.
+        o: tuple
         if isinstance(terms, collections.abc.Iterable):
-            terms = tuple(coerce_formula(phi=term) for term in terms)
-            return super().__new__(cls, terms)
+            elements = tuple(coerce_formula(phi=term) for term in terms)
+            o = super().__new__(cls, elements)
         elif terms is None:
-            return super().__new__(cls)
+            o = super().__new__(cls)
         else:
             raise ValueError()
+        return o
+
+    def __init__(self, c: Connective, terms: FlexibleElements = None):
+        self._c = c
 
     def __repr__(self):
         return self.rep()
@@ -191,9 +150,19 @@ class Terms(tuple):
     def __str__(self):
         return self.rep()
 
+    @property
+    def arity(self) -> int:
+        """The arity of a formula is equal to the number of terms that are direct children of its root node."""
+        return len(self)
+
+    @property
+    def c(self) -> Connective:
+        return self._c
+
     def rep(self, **kwargs) -> str:
         kwargs['parenthesis'] = True
-        return ', '.join(term.rep(**kwargs) for term in self)
+        terms: str = ', '.join(term.rep(**kwargs) for term in self)
+        return f'{self.c.rep(**kwargs)}({terms})'
 
     @property
     def term_0(self) -> Formula:
@@ -205,82 +174,12 @@ class Terms(tuple):
         # TODO: Terms.term_1: raise custom error if there is no term_1
         return self[1]
 
-    def to_terms_builder(self) -> TermsBuilder:
-        """Return a mutable terms-builder that is equivalent to this terms."""
-        terms: tuple[FormulaBuilder, ...] = tuple(coerce_formula_builder(phi=term) for term in self)
-        terms: TermsBuilder = TermsBuilder(terms=terms)
-        return terms
-
-
-class Formula:
-    """An immutable formula modeled as an edge-ordered, node-colored tree."""
-
-    def __init__(self, c: Connective, terms: typing.Optional[Terms] = None):
-        if terms is None:
-            terms = Terms()
-        self._c = c
-        self._terms = terms
-
-    def __repr__(self):
-        return self.rep()
-
-    def __str__(self):
-        return self.rep()
-
-    @property
-    def arity(self) -> int:
-        """The arity of a formula is equal to the number of terms that are direct children of its root node."""
-        return len(self.terms)
-
-    @property
-    def c(self) -> Connective:
-        return self._c
-
-    def rep(self, **kwargs) -> str:
-        kwargs['parenthesis'] = True
-        return f'{self.c.rep(**kwargs)}({self.terms.rep(**kwargs)})'
-
-    @property
-    def term_0(self) -> Formula:
-        """A shortcut for Formula.terms.term_0."""
-        return self.terms.term_0
-
-    @property
-    def term_1(self) -> Formula:
-        return self.terms.term_1
-
-    @property
-    def terms(self) -> Terms:
-        return self._terms
-
     def to_formula_builder(self) -> FormulaBuilder:
         """Returns a formula-builder that is equivalent to this formula.
         This makes it possible to edit the formula-builder to elaborate new formulas."""
-        terms: TermsBuilder = self.terms.to_terms_builder()
+        terms: tuple[FormulaBuilder, ...] = tuple(coerce_formula_builder(phi=term) for term in self)
         phi: FormulaBuilder = FormulaBuilder(c=self.c, terms=terms)
         return phi
-
-
-def coerce_terms(terms: FlexibleTerms = None) -> Terms:
-    if isinstance(terms, Terms):
-        return terms
-    elif isinstance(terms, TermsBuilder):
-        return terms.to_terms()
-    elif terms is None:
-        return Terms()
-    else:
-        return Terms(*terms)
-
-
-def coerce_terms_builder(terms: FlexibleTerms = None):
-    if isinstance(terms, TermsBuilder):
-        return terms
-    elif isinstance(terms, Terms):
-        return terms.to_terms_builder()
-    elif terms is None:
-        return TermsBuilder()
-    else:
-        return TermsBuilder(terms=terms)
 
 
 def coerce_formula_builder(phi: FlexibleFormula = None):
@@ -308,7 +207,15 @@ def coerce_formula(phi: FlexibleFormula):
 
 
 FlexibleFormula = typing.Optional[typing.Union[Connective, Formula, FormulaBuilder]]
-FlexibleTerms = typing.Optional[typing.Union[typing.Iterable, Terms, TermsBuilder]]
+FlexibleElements = typing.Optional[typing.Union[typing.Iterable, Formula, FormulaBuilder]]
+
+
+class FreeArityConnective(Connective):
+    """A free-arity connective is a connective without constraint on its arity,
+    that is its number of descendant notes."""
+
+    def __init__(self, rep: str):
+        super().__init__(rep=rep)
 
 
 class FixedArityConnective(Connective):
@@ -415,11 +322,25 @@ def let_x_be_a_unary_connective(rep: str):
     return UnaryConnective(rep=rep)
 
 
+def let_x_be_a_free_arity_connective(rep: str):
+    return FreeArityConnective(rep=rep)
+
+
 class Connectives(typing.NamedTuple):
-    collection: Connective
-    implies: Connective
-    inference_rule: Connective
-    is_a: Connective
+    collection: FreeArityConnective
+    implies: BinaryConnective
+    inference_rule: TernaryConnective
+    is_a: BinaryConnective
+    transformation: TernaryConnective
+
+
+connectives = Connectives(
+    transformation=let_x_be_a_ternary_connective(rep='transformation'),
+    collection=let_x_be_a_free_arity_connective(rep='collection'),
+    implies=let_x_be_a_binary_connective(rep='implies'),
+    inference_rule=let_x_be_a_ternary_connective(rep='inference-rule'),
+    is_a=let_x_be_a_binary_connective(rep='is-a')
+)
 
 
 def is_connective_equivalent(c: Connective, d: Connective) -> bool:
@@ -450,7 +371,7 @@ def is_formula_equivalent(phi: FlexibleFormula, psi: FlexibleFormula) -> bool:
         # Base case
         return True
     elif (is_connective_equivalent(c=phi.c, d=psi.c)) and (phi.arity == psi.arity) and all(
-            is_formula_equivalent(phi=phi_prime, psi=psi_prime) for phi_prime, psi_prime in zip(phi.terms, psi.terms)):
+            is_formula_equivalent(phi=phi_prime, psi=psi_prime) for phi_prime, psi_prime in zip(phi, psi)):
         # Inductive step
         return True
     else:
@@ -458,15 +379,29 @@ def is_formula_equivalent(phi: FlexibleFormula, psi: FlexibleFormula) -> bool:
         return False
 
 
-connectives = Connectives(
-    collection=let_x_be_a_binary_connective(rep=', '),
-    implies=let_x_be_a_binary_connective(rep='implies'),
-    inference_rule=let_x_be_a_ternary_connective(rep='inference-rule'),
-    is_a=let_x_be_a_binary_connective(rep='is-a')
-)
+class CollectionBuilder(FormulaBuilder):
+    """A utility class to help build collections. It is mutable and thus allows edition."""
+
+    def __init__(self, elements: typing.Optional[typing.Iterable[FlexibleFormula, ...]]):
+        elements: FormulaBuilder = FormulaBuilder(elements=elements)
+        super().__init__(c=connectives.collection, terms=elements)
 
 
-class InferenceRuleBuilder(FormulaBuilder):
+class Collection(Formula):
+    """A collection, or collection-formula, is a formula c(t0, t1, ..., tn) where:
+     - c is a node with the collection connective.
+     - ti is a formula.
+
+     The empty-collection is the collection c()."""
+
+    def __init__(self, elements: typing.Optional[Formula] = None):
+        if elements is None:
+            elements = Formula()
+        super().__init__(c=connectives.collection, terms=elements)
+
+
+class TransformationBuilder(FormulaBuilder):
+
     def __init__(self, premises: typing.Optional[typing.Iterable[FlexibleFormula, ...]], conclusion: FlexibleFormula,
                  variables: typing.Optional[typing.Iterable[FlexibleFormula, ...]]):
         premises: FormulaBuilder = FormulaBuilder(c=connectives.collection, terms=premises)
@@ -484,6 +419,25 @@ class InferenceRuleBuilder(FormulaBuilder):
     @property
     def variables(self) -> FormulaBuilder:
         return self.terms[2]
+
+
+class Transformation:
+    """A transformation is a formula t(P, c, V) where:
+     - t has the transformation connective,
+     - P is a collection-formula whose children are called premises,
+     - c is a formula called conclusion,
+     - V is a collection-formula whose children are variables.
+    From a transformation, the following algorithm is derived:
+    Phi --> psi
+    t(Phi) --> psi
+    map every formula in Phi collection in-order with the premises
+    confirm that they are formula-with-variables-equivalent
+    confirm that every variable has strictly one mapped formula
+    map all variables with their respective formulas
+    return the conclusion by substituting variables with formulas
+    # TODO: Transformation: rewrite the above clearly
+    """
+    pass
 
 # x = let_x_be_a_variable(rep='x')
 # x | connectives.is_a | y
--- tests/test_axiomatic_system_1.py
+++ tests/test_axiomatic_system_1.py
@@ -38,34 +38,34 @@ def fb3(c3):
 @pytest.fixture
 def fb4(c1, c2, c3):
     fb1 = as1.FormulaBuilder(c=c1)
-    n1_0 = fb1.append_term(c=c1)
-    n1_0_0 = n1_0.append_term(c=c3)
-    n1_0_1 = n1_0.append_term(c=c2)
-    n1_1 = fb1.append_term(c=c2)
-    n1_2 = fb1.append_term(c=c3)
-    n1_2_0 = n1_2.append_term(c=c1)
-    n1_2_0_0 = n1_2_0.append_term(c=c1)
-    n1_2_0_0_0 = n1_2_0_0.append_term(c=c1)
-    n1_2_0_0_1 = n1_2_0_0.append_term(c=c3)
-    n1_2_0_1 = n1_2_0.append_term(c=c1)
-    n1_2_1 = n1_2.append_term(c=c2)
+    n1_0 = fb1.append(term=c1)
+    n1_0_0 = n1_0.append(term=c3)
+    n1_0_1 = n1_0.append(term=c2)
+    n1_1 = fb1.append(term=c2)
+    n1_2 = fb1.append(term=c3)
+    n1_2_0 = n1_2.append(term=c1)
+    n1_2_0_0 = n1_2_0.append(term=c1)
+    n1_2_0_0_0 = n1_2_0_0.append(term=c1)
+    n1_2_0_0_1 = n1_2_0_0.append(term=c3)
+    n1_2_0_1 = n1_2_0.append(term=c1)
+    n1_2_1 = n1_2.append(term=c2)
     return fb1
 
 
 @pytest.fixture
 def fb5(c1, c2, c3):
     fb1 = as1.FormulaBuilder(c=c1)
-    n1_0 = fb1.append_term(c=c1)
-    n1_0_0 = n1_0.append_term(c=c3)
-    n1_1 = fb1.append_term(c=c2)
-    n1_2 = fb1.append_term(c=c3)
-    n1_2_0 = n1_2.append_term(c=c1)
-    n1_2_0_0 = n1_2_0.append_term(c=c1)
-    n1_2_0_0_0 = n1_2_0_0.append_term(c=c1)
-    n1_2_0_0_1 = n1_2_0_0.append_term(c=c3)
-    n1_2_0_0_2 = n1_2_0_0.append_term(c=c1)
-    n1_2_0_1 = n1_2_0.append_term(c=c1)
-    n1_2_1 = n1_2.append_term(c=c2)
+    n1_0 = fb1.append(term=c1)
+    n1_0_0 = n1_0.append(term=c3)
+    n1_1 = fb1.append(term=c2)
+    n1_2 = fb1.append(term=c3)
+    n1_2_0 = n1_2.append(term=c1)
+    n1_2_0_0 = n1_2_0.append(term=c1)
+    n1_2_0_0_0 = n1_2_0_0.append(term=c1)
+    n1_2_0_0_1 = n1_2_0_0.append(term=c3)
+    n1_2_0_0_2 = n1_2_0_0.append(term=c1)
+    n1_2_0_1 = n1_2_0.append(term=c1)
+    n1_2_1 = n1_2.append(term=c2)
     return fb1
 
 
@@ -108,43 +108,28 @@ class TestFormulaBuilder:
     def test_assure_term(self):
         fb = as1.FormulaBuilder()
         fb.assure_term(i=3)
-        assert len(fb.terms) == 4
+        assert len(fb) == 4
 
     def test_formula_builder(self, c1, c2, c3, fb4):
         assert fb4.c is c1
-        assert fb4.terms[0].c is c1
-        assert fb4.terms[1].c is c2
-        assert fb4.terms[2].c is c3
+        assert fb4[0].c is c1
+        assert fb4[1].c is c2
+        assert fb4[2].c is c3
 
     def test_term_0(self, c1, c2, c3):
-        fb = as1.FormulaBuilder()
-        fb.term_0 = c1
-        assert fb.term_0.c is c1
-        assert fb.terms[0].c is c1
+        fb = as1.FormulaBuilder(c=c1)
+        fb.term_0 = c2
+        assert fb.c is c1
+        assert fb[0].c is c2
 
-    def test_to_formula(self, fb4):
-        phi1 = fb4.to_formula()
-        assert phi1.c is fb4.c
-        assert phi1.terms[0].c is fb4.terms[0].c
-        assert phi1.terms[0].terms[0].c is fb4.terms[0].terms[0].c
-        assert phi1.terms[0].terms[1].c is fb4.terms[0].terms[1].c
-        assert phi1.terms[1].c is fb4.terms[1].c
-        assert phi1.terms[2].c is fb4.terms[2].c
-        assert phi1.terms[2].terms[0].c is fb4.terms[2].terms[0].c
-        assert phi1.terms[2].terms[0].terms[0].c is fb4.terms[2].terms[0].terms[0].c
-        assert phi1.terms[2].terms[0].terms[1].c is fb4.terms[2].terms[0].terms[1].c
-        assert phi1.terms[2].terms[1].c is fb4.terms[2].terms[1].c
-
-
-class TestTermsBuilder:
-    def test_terms_builder(self, fb1, fb2, fb3):
-        terms1 = as1.TermsBuilder()
+    def test_terms(self, fb1, fb2, fb3):
+        terms1 = as1.FormulaBuilder()
         assert len(terms1) == 0
-        terms2 = as1.TermsBuilder(terms=(fb1,))
+        terms2 = as1.FormulaBuilder(terms=(fb1,))
         assert len(terms2) == 1
         assert terms2[0].c is fb1.c
         assert terms2.term_0.c is fb1.c
-        terms3 = as1.TermsBuilder(terms=(fb1, fb2, fb1, fb1, fb3))
+        terms3 = as1.FormulaBuilder(terms=(fb1, fb2, fb1, fb1, fb3))
         assert len(terms3) == 5
         assert terms3[0].c is fb1.c
         assert terms3.term_0.c is fb1.c
@@ -154,16 +139,43 @@ class TestTermsBuilder:
         assert terms3[3].c is fb1.c
         assert terms3[4].c is fb3.c
 
+    def test_to_formula(self, fb4):
+        phi1 = fb4.to_formula()
+        assert phi1.c is fb4.c
+        assert phi1[0].c is fb4[0].c
+        assert phi1[0][0].c is fb4[0][0].c
+        assert phi1[0][1].c is fb4[0][1].c
+        assert phi1[1].c is fb4[1].c
+        assert phi1[2].c is fb4[2].c
+        assert phi1[2][0].c is fb4[2][0].c
+        assert phi1[2][0][0].c is fb4[2][0][0].c
+        assert phi1[2][0][1].c is fb4[2][0][1].c
+        assert phi1[2][1].c is fb4[2][1].c
 
-class TestTerms:
-    def test_terms(self, phi1, phi2, phi3):
-        terms1 = as1.Terms()
+
+class TestFormula:
+
+    def test_formula(self, c1, c2, c3, phi4):
+        assert phi4.c is c1
+        assert phi4[0].c is c1
+        assert phi4[1].c is c2
+        assert phi4[2].c is c3
+
+    def test_term_1(self, c1, c2, c3):
+        fb = as1.FormulaBuilder()
+        fb.term_0 = c1
+        phi = fb.to_formula()
+        assert phi.term_0.c is c1
+        assert phi[0].c is c1
+
+    def test_terms(self, c1, phi1, phi2, phi3):
+        terms1 = as1.Formula(c=c1)
         assert len(terms1) == 0
-        terms2 = as1.Terms(terms=(phi1,))
+        terms2 = as1.Formula(c=c1, terms=(phi1,))
         assert len(terms2) == 1
         assert terms2[0].c is phi1.c
         assert terms2.term_0.c is phi1.c
-        terms3 = as1.Terms(terms=(phi1, phi2, phi1, phi1, phi3))
+        terms3 = as1.Formula(c=c1, terms=(phi1, phi2, phi1, phi1, phi3))
         assert len(terms3) == 5
         assert terms3[0].c is phi1.c
         assert terms3.term_0.c is phi1.c
@@ -173,34 +185,18 @@ class TestTerms:
         assert terms3[3].c is phi1.c
         assert terms3[4].c is phi3.c
 
-
-class TestFormula:
-
-    def test_formula(self, c1, c2, c3, phi4):
-        assert phi4.c is c1
-        assert phi4.terms[0].c is c1
-        assert phi4.terms[1].c is c2
-        assert phi4.terms[2].c is c3
-
-    def test_term_1(self, c1, c2, c3):
-        fb = as1.FormulaBuilder()
-        fb.term_0 = c1
-        phi = fb.to_formula()
-        assert phi.term_0.c is c1
-        assert phi.terms[0].c is c1
-
     def test_to_formula_builder(self, phi4):
         fb1 = phi4.to_formula_builder()
         assert fb1.c is phi4.c
-        assert fb1.terms[0].c is phi4.terms[0].c
-        assert fb1.terms[0].terms[0].c is phi4.terms[0].terms[0].c
-        assert fb1.terms[0].terms[1].c is phi4.terms[0].terms[1].c
-        assert fb1.terms[1].c is phi4.terms[1].c
-        assert fb1.terms[2].c is phi4.terms[2].c
-        assert fb1.terms[2].terms[0].c is phi4.terms[2].terms[0].c
-        assert fb1.terms[2].terms[0].terms[0].c is phi4.terms[2].terms[0].terms[0].c
-        assert fb1.terms[2].terms[0].terms[1].c is phi4.terms[2].terms[0].terms[1].c
-        assert fb1.terms[2].terms[1].c is phi4.terms[2].terms[1].c
+        assert fb1[0].c is phi4[0].c
+        assert fb1[0][0].c is phi4[0][0].c
+        assert fb1[0][1].c is phi4[0][1].c
+        assert fb1[1].c is phi4[1].c
+        assert fb1[2].c is phi4[2].c
+        assert fb1[2][0].c is phi4[2][0].c
+        assert fb1[2][0][0].c is phi4[2][0][0].c
+        assert fb1[2][0][1].c is phi4[2][0][1].c
+        assert fb1[2][1].c is phi4[2][1].c
 
 
 class TestConnectiveEquivalence:
