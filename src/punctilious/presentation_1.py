df75015de2fd7733931d0dc477a9814d36cb9277
daviddoret <david.doret@me.com>
2024-05-26 15:14:01 +0200

    version 2.0: re-starting from scratch with a new data model.

--- src/punctilious/presentation_1.py
+++ src/punctilious/presentation_1.py
@@ -39,13 +39,17 @@ class Encoding:
     """
 
     def __init__(self, key: str):
-        self._name = key
+        self._key = key
 
     def __repr__(self):
-        return self._name
+        return self._key
 
     def __str__(self):
-        return self._name
+        return self._key
+
+    @property
+    def key(self) -> str:
+        return self._key
 
 
 class Encodings(dict):
@@ -60,11 +64,21 @@ class Encodings(dict):
     def __init__(self):
         global configuration_settings
         super().__init__()
-        self._latex_math = self._register(key='latex_math')
-        self._unicode_extended = self._register(key='unicode_extended')
-        self._unicode_limited = self._register(key='unicode_limited')
+        self._latex_math = self._register(Encoding(key='latex_math'))
+        self._unicode_extended = self._register(Encoding(key='unicode_extended'))
+        self._unicode_limited = self._register(Encoding(key='unicode_limited'))
         default_encoding = configuration_settings['presentation_1']['default_encoding']
         self._default = self[default_encoding]
+
+    def __repr__(self):
+        return 'encodings'
+
+    def __str__(self):
+        return 'encodings'
+
+    def _register(self, encoding: Encoding):
+        self[encoding.key] = encoding
+        return encoding
 
     @property
     def default(self):
@@ -84,11 +98,6 @@ class Encodings(dict):
     @property
     def latex_math(self):
         return self._latex_math
-
-    def _register(self, key: str):
-        encoding: Encoding = Encoding(key=key)
-        self[key] = encoding
-        return encoding
 
     @property
     def unicode_extended(self):
@@ -117,8 +126,18 @@ class TypesettingMethod(abc.ABC):
      - typeset_from_generator(...)
      """
 
-    def __init__(self):
-        pass
+    def __init__(self, key: str):
+        self._key = key
+
+    def __repr__(self) -> str:
+        return self.typeset_as_string(encoding=None)
+
+    def __str__(self) -> str:
+        return self.typeset_as_string(encoding=None)
+
+    @property
+    def key(self):
+        return self._key
 
     def typeset_as_string(self, encoding: typing.Optional[Encoding] = None, **kwargs) -> str:
         """Returns a python-string from the typesetting-method.
@@ -140,8 +159,48 @@ class TypesettingMethod(abc.ABC):
         raise NotImplementedError('This is an abstract method.')
 
 
+class FailsafeTypesettingMethod(TypesettingMethod):
+    def __init__(self):
+        super().__init__(key='failsafe-typesetting-method')
+
+    def typeset_as_string(self, encoding: typing.Optional[Encoding] = None, **kwargs) -> str:
+        return f'python-object-{id(self)}'
+
+    def typeset_from_generator(self, encoding: typing.Optional[Encoding] = None, **kwargs) -> (
+            typing.Generator)[str, None, None]:
+        yield from self.typeset_as_string(encoding=encoding, **kwargs)
+
+
+class TypesettingMethods(dict):
+    """A catalog of out-of-the-box encodings."""
+    _singleton = None
+
+    def __new__(cls):
+        if cls._singleton is None:
+            cls._singleton = super(TypesettingMethods, cls).__new__(cls)
+        return cls._singleton
+
+    def __init__(self):
+        global configuration_settings
+        super().__init__()
+        self._failsafe = self._register(typesetting_method=FailsafeTypesettingMethod())
+
+    def _register(self, typesetting_method: TypesettingMethod) -> TypesettingMethod:
+        self[typesetting_method.key] = typesetting_method
+        return typesetting_method
+
+    @property
+    def failsafe(self):
+        return self._failsafe
+
+
+typesetting_methods = TypesettingMethods()
+
+
 class Typesettable:
-    """A typesettable is a python object equipped with a reusable typesetting-configuration."""
+    """A typesettable is a python object equipped with a reusable typesetting-configuration.
+
+    By inheriting from Typesettable, typesetting capabilities are added to the original class."""
 
     def __init__(self, typesetting_configuration: typing.Optional[TypesettingConfiguration] = None):
         if typesetting_configuration is None:
@@ -199,6 +258,12 @@ class TypesettingConfiguration:
     def __init__(self, typesetting_method: typing.Optional[TypesettingMethod] = None):
         self._typesetting_method: typing.Optional[TypesettingMethod] = typesetting_method
 
+    def __repr__(self) -> str:
+        return self.typeset_as_string(encoding=None)
+
+    def __str__(self) -> str:
+        return self.typeset_as_string(encoding=None)
+
     def typeset_as_string(self, encoding: typing.Optional[Encoding] = None, **kwargs) -> str:
         """Returns a python-string from the typesetting-method.
 
@@ -206,10 +271,11 @@ class TypesettingConfiguration:
         If the typesetting-method returns too lengthy content, you may prefer typeset_from_generator() to avoid
         loading all the content in memory.
         """
+        global typesetting_methods
         if self.typesetting_method is not None:
             return self.typesetting_method.typeset_as_string(encoding=encoding, **kwargs)
         else:
-            raise ValueError('Not typesetting-method available')
+            return typesetting_methods.failsafe.typeset_as_string(encoding=encoding, **kwargs)
 
     def typeset_from_generator(self, encoding: typing.Optional[Encoding] = None, **kwargs) -> (
             typing.Generator)[str, None, None]:
@@ -222,7 +288,7 @@ class TypesettingConfiguration:
         if self.typesetting_method is not None:
             yield from self.typesetting_method.typeset_as_string(encoding=encoding, **kwargs)
         else:
-            raise ValueError('Not typesetting-method available')
+            yield from typesetting_methods.failsafe.typeset_as_string(encoding=encoding, **kwargs)
 
     @property
     def typesetting_method(self) -> typing.Optional[TypesettingMethod]:
@@ -236,11 +302,11 @@ class TypesettingConfiguration:
 class Symbol(TypesettingMethod):
     """An atomic symbol."""
 
-    def __init__(self, latex_math: str, unicode_extended: str, unicode_limited: str):
+    def __init__(self, key: str, latex_math: str, unicode_extended: str, unicode_limited: str):
         self._latex_math: str = latex_math
         self._unicode_extended: str = unicode_extended
         self._unicode_limited: str = unicode_limited
-        super().__init__()
+        super().__init__(key=key)
 
     @property
     def latex_math(self) -> str:
@@ -259,6 +325,16 @@ class Symbol(TypesettingMethod):
         else:
             raise ValueError('ooops')
 
+    def typeset_from_generator(self, encoding: typing.Optional[Encoding] = None, **kwargs) -> (
+            typing.Generator)[str, None, None]:
+        """Generates a stream of python-string chunks from the typesetting-method.
+
+        If the typesetting-method returns content of reasonable length, typeset_as_string() is an adequate solution.
+        If the typesetting-method returns too lengthy content, you may prefer typeset_from_generator() to avoid
+        loading all the content in memory.
+        """
+        yield self.typeset_as_string(encoding=encoding, **kwargs)
+
     @property
     def unicode_extended(self) -> str:
         return self._unicode_extended
@@ -268,7 +344,7 @@ class Symbol(TypesettingMethod):
         return self._unicode_limited
 
 
-class Symbols(list):
+class Symbols(dict):
     """A catalog of out-of-the-box symbols."""
     _singleton = None
 
@@ -279,20 +355,33 @@ class Symbols(list):
 
     def __init__(self):
         super().__init__()
-        self._asterisk_operator = self.register(latex_math='\\ast', unicode_extended='∗', unicode_limited='*')
-        self._close_parenthesis = self.register(latex_math='\\right)', unicode_extended=')', unicode_limited=')')
-        self._collection_separator = self.register(latex_math=', ', unicode_extended=', ', unicode_limited=', ')
-        self._not_sign = self.register(latex_math='\\lnot', unicode_extended='¬', unicode_limited='not')
-        self._open_parenthesis = self.register(latex_math='\\left(', unicode_extended='(', unicode_limited='(')
-        self._p_uppercase_serif_italic = self.register(latex_math='\\textit{P}', unicode_extended='𝑃',
-                                                       unicode_limited='P')
-        self._q_uppercase_serif_italic = self.register(latex_math='\\textit{Q}', unicode_extended='𝑄',
-                                                       unicode_limited='Q')
-        self._r_uppercase_serif_italic = self.register(latex_math='\\textit{R}', unicode_extended='𝑅',
-                                                       unicode_limited='R')
-        self._rightwards_arrow = self.register(latex_math='\\rightarrow', unicode_extended='→', unicode_limited='-->')
-        self._space = self.register(latex_math=' ', unicode_extended=' ', unicode_limited=' ')
-        self._tilde = self.register(latex_math='\\sim', unicode_extended='~', unicode_limited='~')
+        self._asterisk_operator = self._register(
+            Symbol(key='asterisk_operator', latex_math='\\ast', unicode_extended='∗', unicode_limited='*'))
+        self._close_parenthesis = self._register(
+            Symbol(key='close_parenthesis', latex_math='\\right)', unicode_extended=')', unicode_limited=')'))
+        self._collection_separator = self._register(
+            Symbol(key='collection_separator', latex_math=', ', unicode_extended=', ', unicode_limited=', '))
+        self._not_sign = self._register(
+            Symbol(key='not_sign', latex_math='\\lnot', unicode_extended='¬', unicode_limited='not'))
+        self._open_parenthesis = self._register(
+            Symbol(key='open_parenthesis', latex_math='\\left(', unicode_extended='(', unicode_limited='('))
+        self._p_uppercase_serif_italic = self._register(
+            Symbol(key='p_uppercase_serif_italic', latex_math='\\textit{P}', unicode_extended='𝑃',
+                   unicode_limited='P'))
+        self._q_uppercase_serif_italic = self._register(
+            Symbol(key='q_uppercase_serif_italic', latex_math='\\textit{Q}', unicode_extended='𝑄',
+                   unicode_limited='Q'))
+        self._r_uppercase_serif_italic = self._register(
+            Symbol(key='r_uppercase_serif_italic', latex_math='\\textit{R}', unicode_extended='𝑅',
+                   unicode_limited='R'))
+        self._rightwards_arrow = self._register(
+            Symbol(key='rightwards_arrow', latex_math='\\rightarrow', unicode_extended='→', unicode_limited='-->'))
+        self._space = self._register(Symbol(key='space', latex_math=' ', unicode_extended=' ', unicode_limited=' '))
+        self._tilde = self._register(Symbol(key='tilde', latex_math='\\sim', unicode_extended='~', unicode_limited='~'))
+
+    def _register(self, symbol: Symbol):
+        self[symbol.key] = symbol
+        return symbol
 
     @property
     def asterisk_operator(self) -> Symbol:
@@ -321,11 +410,6 @@ class Symbols(list):
     @property
     def q_uppercase_serif_italic(self) -> Symbol:
         return self._q_uppercase_serif_italic
-
-    def register(self, latex_math: str, unicode_extended: str, unicode_limited: str):
-        symbol = Symbol(latex_math=latex_math, unicode_extended=unicode_extended, unicode_limited=unicode_limited)
-        self.append(symbol)
-        return symbol
 
     @property
     def r_uppercase_serif_italic(self) -> Symbol:
--- tests/test_presentation_1.py
+++ tests/test_presentation_1.py
@@ -5,7 +5,7 @@ import punctilious as pu
 class TestTypesettingConfiguration:
     def test_typesetting_configuration(self):
         tc = pu.p1.Typesettable()
-        tc.typesetting_method = pu.p1.symbols.p_uppercase_serif_italic
-        assert tc.rep(encoding=pu.p1.encodings.latex_math) == '\\textit{P}'
-        assert tc.rep(encoding=pu.p1.encodings.unicode_extended) == '𝑃'
-        assert tc.rep(encoding=pu.p1.encodings.unicode_limited) == 'P'
+        tc.typesetting_configuration.typesetting_method = pu.p1.symbols.p_uppercase_serif_italic
+        assert tc.typeset_as_string(encoding=pu.p1.encodings.latex_math) == '\\textit{P}'
+        assert tc.typeset_as_string(encoding=pu.p1.encodings.unicode_extended) == '𝑃'
+        assert tc.typeset_as_string(encoding=pu.p1.encodings.unicode_limited) == 'P'
