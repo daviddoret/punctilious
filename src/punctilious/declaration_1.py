333d4305374e40b8bd994a38e210ccbfde4f2acd
daviddoret <david.doret@me.com>
2024-07-14 18:07:20 +0200

    version 2.0: re-starting from scratch with a new data model.

---
+++ src/punctilious/declaration_1.py
@@ -0,0 +1,20 @@
+# TODO: The idea is to develop here a meta-theory that allows the usage of the := connective to declare
+#   definitions.
+
+import presentation_layer_1 as pl1
+import axiomatic_system_1 as as1
+
+is_defined_as: as1.Connective = as1.BinaryConnective(formula_ts=':=')
+
+# inference-rule:
+# premises:
+#   - is-a-symbol(s)
+#   - s := phi
+# conclusion:
+#   d is any derivation
+#   substitute(d,s,phi)
+
+# idea:
+#  - a static conclusion is not adequate, perhaps we could use a dynamic conclusion,
+#     like a python-function conclusion_is_potentially_compatible_with_target(...) ???
+#     is this worthwhile? or are we better off just computing the effective conclusions?
