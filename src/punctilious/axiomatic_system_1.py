e47dfc743f9167041e799549123c4b418756e1b4
daviddoret <david.doret@me.com>
2024-07-16 05:48:51 +0200

    version 2.0: re-starting from scratch with a new data model.

--- src/punctilious/axiomatic_system_1.py
+++ src/punctilious/axiomatic_system_1.py
@@ -434,7 +434,8 @@ def coerce_enumeration_of_variables(e: FlexibleEnumeration) -> Enumeration:
     return e2
 
 
-def union_enumeration(phi: FlexibleEnumeration, psi: FlexibleEnumeration, strip_duplicates: bool = True) -> Enumeration:
+def union_enumeration(phi: FlexibleEnumeration, psi: FlexibleEnumeration, strip_duplicates: bool = True,
+                      interpret_none_as_empty: bool = True, canonic_conversion: bool = True) -> Enumeration:
     """Given two enumerations phi, and psi, the union-enumeration operator, noted phi ∪-enumeration psi,
     returns a new enumeration omega such that:
     - all elements of phi are elements of omega,
@@ -453,9 +454,56 @@ def union_enumeration(phi: FlexibleEnumeration, psi: FlexibleEnumeration, strip_
      - Idempotent: (phi ∪-formula phi) ~formula phi.
      - Not symmetric if some element of psi are elements of phi: because of order.
     """
-    phi: Enumeration = coerce_enumeration(e=phi, interpret_none_as_empty=True, strip_duplicates=strip_duplicates)
-    psi: Enumeration = coerce_enumeration(e=psi, interpret_none_as_empty=True, strip_duplicates=strip_duplicates)
+    phi: Enumeration = coerce_enumeration(e=phi, interpret_none_as_empty=interpret_none_as_empty,
+                                          strip_duplicates=strip_duplicates,
+                                          canonic_conversion=canonic_conversion)
+    psi: Enumeration = coerce_enumeration(e=psi, interpret_none_as_empty=interpret_none_as_empty,
+                                          strip_duplicates=strip_duplicates,
+                                          canonic_conversion=canonic_conversion)
     e: Enumeration = Enumeration(e=(*phi, *psi,), strip_duplicates=strip_duplicates)
+    return e
+
+
+def intersection_enumeration(phi: FlexibleEnumeration, psi: FlexibleEnumeration, strip_duplicates: bool = True,
+                             interpret_none_as_empty: bool = True, canonic_conversion: bool = True) -> Enumeration:
+    """Given two enumerations phi, and psi, the intersection-enumeration operator, noted phi ∩-enumeration psi,
+    returns a new enumeration omega such that:
+    - all elements of the resulting enumeration are elements of phi and of psi.
+    Order is preserved, that is:
+    - the elements from phi keep their original relative order
+
+    Under enumeration-equivalence, the intersection-enumeration operator is:
+     - Idempotent: (phi ∩-enumeration phi) ~enumeration phi.
+     - Symmetric: (phi ∩-enumeration psi) ~enumeration (psi ∩-enumeration phi).
+    """
+    phi: Enumeration = coerce_enumeration(e=phi, interpret_none_as_empty=interpret_none_as_empty,
+                                          strip_duplicates=strip_duplicates,
+                                          canonic_conversion=canonic_conversion)
+    psi: Enumeration = coerce_enumeration(e=psi, interpret_none_as_empty=interpret_none_as_empty,
+                                          strip_duplicates=strip_duplicates,
+                                          canonic_conversion=canonic_conversion)
+    common_elements: list = [x for x in iterate_enumeration_elements(e=phi) if is_element_of_enumeration(x=x, e=psi)]
+    e: Enumeration = Enumeration(e=common_elements, strip_duplicates=strip_duplicates)
+    return e
+
+
+def difference_enumeration(phi: FlexibleEnumeration, psi: FlexibleEnumeration, strip_duplicates: bool = True,
+                           interpret_none_as_empty: bool = True, canonic_conversion: bool = True) -> Enumeration:
+    """Given two enumerations phi, and psi, the difference-enumeration operator, noted phi ∖-enumeration psi,
+    returns a new enumeration omega such that:
+    - all elements of the resulting enumeration are elements of phi but not psi.
+    Order is preserved, that is:
+    - the elements from phi keep their original relative order
+    """
+    phi: Enumeration = coerce_enumeration(e=phi, interpret_none_as_empty=interpret_none_as_empty,
+                                          strip_duplicates=strip_duplicates,
+                                          canonic_conversion=canonic_conversion)
+    psi: Enumeration = coerce_enumeration(e=psi, interpret_none_as_empty=interpret_none_as_empty,
+                                          strip_duplicates=strip_duplicates,
+                                          canonic_conversion=canonic_conversion)
+    different_elements: list = [x for x in iterate_enumeration_elements(e=phi) if
+                                not is_element_of_enumeration(x=x, e=psi)]
+    e: Enumeration = Enumeration(e=different_elements, strip_duplicates=strip_duplicates)
     return e
 
 
@@ -2740,9 +2788,9 @@ def is_well_formed_derivation(d: FlexibleFormula) -> bool:
 
 
 def would_be_valid_derivations_in_theory(u: FlexibleEnumeration, v: FlexibleTheory,
-                                         raise_error_if_false: bool = False) -> bool:
-    """Given an enumeration of presumably verified derivations "v" (e.g.: that reflects a theory "t"),
-    and a complementary enumeration of unverified derivations "u" (e.g.: whose elements are not (yet) effective
+                                         raise_error_if_false: bool = False) -> tuple[bool, Enumeration]:
+    """Given an enumeration of presumably verified derivations "v" (e.g.: the derivation sequence of a theory "t"),
+    and an enumeration of unverified derivations "u" (e.g.: whose elements are not (yet) effective
     theorems of "t"), returns True if a theory would be well-formed if it was composed of
     derivations "u" appended to derivations "v", or False if it would not.
 
@@ -2752,22 +2800,30 @@ def would_be_valid_derivations_in_theory(u: FlexibleEnumeration, v: FlexibleTheo
     :param u: An enumeration of derivations.
     :param v: A theory.
     :param raise_error_if_false:
-    :return:
+    :return: A pair (b, u′) where "b" is True if all derivations in "u" would be valid, False otherwise, and "u′" is the
+        enumeration of the newly verified derivation steps if "b" is True, None otherwise. Note that u′ = u ∖ v because
+        derivation steps must be unique to be a well-formed derivation sequence.
     """
     u: Enumeration = coerce_enumeration(e=u, strip_duplicates=True, interpret_none_as_empty=True,
                                         canonic_conversion=True)
     v: Enumeration = coerce_enumeration(e=v, strip_duplicates=True, interpret_none_as_empty=True,
                                         canonic_conversion=True)
 
+    # Consider only unverified derivations that are not verified.
+    # In effect, a derivation sequence must contain unique derivations under enumeration-equivalence.
+    u2: Enumeration = difference_enumeration(phi=u, psi=v, strip_duplicates=True, interpret_none_as_empty=True,
+                                             canonic_conversion=True)
+
     # Create a complete enumeration "c" composed of derivations "u" appended to derivations "v",
     # getting rid of duplicates if any in the process.
-    c: Enumeration = union_enumeration(phi=v, psi=u, strip_duplicates=True)
+    c: Enumeration = union_enumeration(phi=v, psi=u2, strip_duplicates=True)
 
     # Put aside the index from which the proofs of derivations have not been verified.
     verification_threshold: int = len(v)
 
     # Coerce all enumeration elements to axioms, inference-rules, and theorems.
-    c: Enumeration = Enumeration(e=coerce_derivation(d=d) for d in iterate_enumeration_elements(e=c))
+    coerced_elements: list = [coerce_derivation(d=d) for d in iterate_enumeration_elements(e=c)]
+    c: Enumeration = Enumeration(e=coerced_elements)
 
     # Iterate through all index positions of derivations for which the proofs must be verified.
     for index in range(verification_threshold, len(c)):
@@ -2778,78 +2834,72 @@ def would_be_valid_derivations_in_theory(u: FlexibleEnumeration, v: FlexibleTheo
         # Retrieve the proposition or statement announced by the derivation.
         p: Formula = d.valid_statement
 
-        # Assume verification failure.
-        p_verification_success: bool = False
-
         if is_well_formed_axiom(a=d):
             # This is an axiom.
             # By definition, the presence of an axiom in a theory is valid.
-            p_verification_success = True
+            pass
         elif is_well_formed_inference_rule(i=d):
             # This is an inference-rule.
             # By definition, the presence of an inference-rule in a theory is valid.
-            p_verification_success = True
+            pass
         elif is_well_formed_theorem(t=d):
             # This is a well-formed theorem.
             # Check that this theorem is a valid derivation with regard to predecessor theorems.
             m: Theorem = coerce_theorem(t=d)
             i: Inference = m.inference
             ir: InferenceRule = m.inference.inference_rule
-            # Check that the inference-rule is valid in the theory.
-            if not any(
-                    is_well_formed_inference_rule(i=c[x]) and
-                    is_formula_equivalent(phi=ir, psi=c[x]) for x in range(0, index)):
+            # Check that the inference-rule is a valid predecessor in the derivation.
+            if not any(is_formula_equivalent(phi=ir, psi=c[x]) for x in range(0, index)):
                 if raise_error_if_false:
                     raise u1.ApplicativeError(
                         code=c1.ERROR_CODE_AS1_068,
-                        msg='No element "c[x]" is a well-formed inference-rule and'
-                            'formula-equivalent to inference-rule "ir",'
-                            'where "x" < "index".',
-                        ir=ir, index=index, c=c)
-                return False
-            # Check that all premises are valid propositions in the theory.
-            for p in i.premises:
-                # Check that this premise is a valid proposition in the theory.
-                p_validity, p_index = is_valid_proposition_in_theory_2(p=p, t=v)
-                if not p_validity:
-                    # The premise is not a valid proposition in the theory.
+                        msg='Inference-rule "ir" is not a valid predecessor (with index strictly less than "index").'
+                            ' This forbids the derivation of proposition "p" in step "d" in the derivation sequence.',
+                        p=p, ir=ir, index=index, d=d, c=c)
+                return False, _
+            # Check that all premises are valid predecessor propositions in the derivation.
+            for q in i.premises:
+                # Check that this premise is a valid predecessor proposition in the derivation.
+                if not any(is_formula_equivalent(phi=q, psi=c[x]) for x in range(0, index)):
                     if raise_error_if_false:
                         raise u1.ApplicativeError(
-                            msg='Proposition "p" is not valid in theory "t".',
-                            code=c1.ERROR_CODE_AS1_036, p_validity=p_validity,
-                            p=p, t=v)
-                    return False
-                else:
-                # The premise is valid.
-                # Check that
-                # TODO: CHECK THAT THE INDEX OF THE VALID STATEMENT < THE INDEX OF P
-                XXXXX
-                is_valid, premise_index = is_valid_statement_in_theory_2(phi=p, t=v)
-            # Finally, check that the inference-rule effectively yields the
-            # announced proposition.
+                            msg='Premise "q" is not a valid predecessor (with index strictly less than "index").'
+                                ' This forbids the derivation of proposition "p" in step "d" in the derivation'
+                                ' sequence.',
+                            code=c1.ERROR_CODE_AS1_036,
+                            p=p, q=q, index=index, d=d, c=c)
+                    return False, _
+            # Check that the transformation of the inference-rule effectively yields the announced proposition.
             t2: Transformation = i.inference_rule.transformation
             p_prime = t2.apply_transformation(p=i.premises, a=i.arguments)
             if not is_formula_equivalent(phi=p, psi=p_prime):
                 if raise_error_if_false:
                     raise u1.ApplicativeError(
-                        msg='Transformation "t2" of inference-rule "ir" does not yield "p" but it yields "p_prime".'
-                            'The inference "i" contains the premises and the arguments.'
-                            'In conclusion, derivation "d" would not be valid if it was appended to theory "t".',
+                        msg='Transformation "t2" of inference-rule "ir" does not yield the expected proposition "p",'
+                            ' but yields "p_prime".'
+                            ' This forbids the derivation of proposition "p" in step "d" in the derivation sequence.'
+                            ' Inference "i" contains the arguments (premises and the complementary arguments).',
                         code=c1.ERROR_CODE_AS1_036,
-                        p=p, p_prime=p_prime, t2=t2, ir=ir, i=i, t=v)
-                return False
+                        p=p, p_prime=p_prime, index=index, t2=t2, ir=ir, i=i, d=d, c=c)
+                return False, _
             # All tests have been successfully completed, we now have the assurance
             # that derivation "d" would be valid if appended to theory "t".
-            return True
+            pass
         else:
             # Incorrect form.
             if raise_error_if_false:
                 raise u1.ApplicativeError(
                     msg='Expected derivation "d" is not of a proper form (e.g. axiom, inference-rule or theorem).'
-                        'In conclusion, derivation "d" would not be valid if it was appended to theory "t".',
+                        ' This forbids the derivation of proposition "p" in step "d" in the derivation'
+                        ' sequence.',
                     code=c1.ERROR_CODE_AS1_071,
-                    d=d, t=v)
-            return False
+                    p=p, d=d, index=index, c=c)
+            return False, _
+        # Derivation "d" is valid.
+        pass
+    # All unverified derivations have been verified.
+    pass
+    return True, u2
 
 
 def is_well_formed_theory(t: FlexibleFormula, raise_event_if_false: bool = False) -> bool:
