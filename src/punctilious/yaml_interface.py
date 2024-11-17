7fdb3fbf223ee95986665da373a5a9705520637c
daviddoret <david.doret@me.com>
2024-11-17 18:24:15 +0100

    Lark + yaml

--- src/punctilious/yaml_interface.py
+++ src/punctilious/yaml_interface.py
@@ -28,22 +28,76 @@ def get_logger():
 logger = get_logger()
 
 
-class Import:
-    def __init__(self, local_name, source, method):
-        self.local_name = local_name
-        self.source = source
-        self.method = method
+class Imports(tuple):
+
+    def __init__(self, *args):
+        pass
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls, args)
 
     def __repr__(self):
-        return self.to_yaml(default_flow_style=True)
+        return '(' + ', '.join(str(e) for e in self) + ')'
 
     def __str__(self):
-        return self.to_yaml(default_flow_style=True)
+        return '(' + ', '.join(str(e) for e in self) + ')'
 
-    def to_raw(self):
+    @classmethod
+    def instantiate_from_list(cls, l: list):
+        typed_l = []
+        for d in l:
+            o = Import.instantiate_from_dict(d=d)
+            typed_l.append(o)
+        return Imports(*typed_l)
+
+    def to_yaml(self, default_flow_style):
+        return yaml.dump(self, default_flow_style=default_flow_style)
+
+
+class Import:
+    __slots__ = ('_slug', '_source', '_method')
+
+    def __hash__(self):
+        # hash only spans the properties that uniquely identify the object.
+        return hash((self.__class__, self._slug, self._source))
+
+    def __init__(self, slug, source, method):
+        self._slug = slug
+        self._source = source
+        self._method = method
+
+    def __repr__(self):
+        return self.slug
+
+    def __str__(self):
+        return self.slug
+
+    @classmethod
+    def instantiate_from_dict(cls, d: dict | None):
+        if d is None:
+            d = {}
+        slug = d['slug'] if 'slug' in d.keys() else None
+        source = d['source'] if 'source' in d.keys() else None
+        method = d['method'] if 'method' in d.keys() else None
+        o = Import(slug=slug, source=source, method=method)
+        return o
+
+    @property
+    def method(self):
+        return self._method
+
+    @property
+    def slug(self):
+        return self._slug
+
+    @property
+    def source(self):
+        return self._source
+
+    def to_dict(self):
         d = {}
-        if self.local_name is not None:
-            d['local_name'] = self.local_name
+        if self.slug is not None:
+            d['local_name'] = self.slug
         if self.source is not None:
             d['source'] = self.source
         if self.method is not None:
@@ -51,7 +105,7 @@ class Import:
         return d
 
     def to_yaml(self, default_flow_style):
-        yaml.dump(self.to_raw(), default_flow_style=default_flow_style)
+        yaml.dump(self.to_dict(), default_flow_style=default_flow_style)
 
 
 class SyntacticRules:
@@ -82,16 +136,12 @@ class SyntacticRules:
         yaml.dump(self.to_dict(), default_flow_style=default_flow_style)
 
     @classmethod
-    def load_from_raw(cls, raw: dict):
-        fixed_arity = None
-        min_arity = None
-        max_arity = None
-        if 'fixed_arity' in raw.keys():
-            fixed_arity = raw['fixed_arity']
-        if 'min_arity' in raw.keys():
-            min_arity = raw['min_arity']
-        if 'max_arity' in raw.keys():
-            max_arity = raw['max_arity']
+    def instantiate_from_list(cls, d: dict):
+        if d is None:
+            d = {}
+        fixed_arity = d['fixed_arity'] if 'fixed_arity' in d.keys() else None
+        min_arity = d['min_arity'] if 'min_arity' in d.keys() else None
+        max_arity = d['max_arity'] if 'max_arity' in d.keys() else None
         o = SyntacticRules(fixed_arity=fixed_arity, min_arity=min_arity, max_arity=max_arity)
         return o
 
@@ -99,6 +149,10 @@ class SyntacticRules:
 class Configuration:
     """"""
     __slots__ = ('_mode', '_language', '_encoding', '_template')
+
+    def __hash__(self):
+        # hash only spans the properties that uniquely identify the object.
+        return hash((self.__class__, self._mode, self._language, self._encoding))
 
     def __init__(self, mode=None, language=None, encoding=None, template=None):
         self._mode = mode
@@ -121,13 +175,13 @@ class Configuration:
         return self._language
 
     @classmethod
-    def load_from_raw(cls, raw: dict | None):
-        if raw is None:
-            raw = {}
-        mode = raw['mode'] if 'mode' in raw.keys() else None
-        language = raw['language'] if 'language' in raw.keys() else None
-        encoding = raw['encoding'] if 'encoding' in raw.keys() else None
-        template = raw['template'] if 'template' in raw.keys() else None
+    def instantiate_from_dict(cls, d: dict | None):
+        if d is None:
+            d = {}
+        mode = d['mode'] if 'mode' in d.keys() else None
+        language = d['language'] if 'language' in d.keys() else None
+        encoding = d['encoding'] if 'encoding' in d.keys() else None
+        template = d['template'] if 'template' in d.keys() else None
         o = Configuration(mode=mode, language=language, encoding=encoding, template=template)
         return o
 
@@ -139,7 +193,7 @@ class Configuration:
     def template(self):
         return self._template
 
-    def to_raw(self):
+    def to_dict(self):
         d = {}
         if self.mode is not None:
             d['mode'] = self.mode
@@ -152,7 +206,7 @@ class Configuration:
         return d
 
     def to_yaml(self, default_flow_style):
-        return yaml.dump(self.to_raw(), default_flow_style=default_flow_style)
+        return yaml.dump(self.to_dict(), default_flow_style=default_flow_style)
 
 
 class Configurations(tuple):
@@ -170,12 +224,12 @@ class Configurations(tuple):
         return '(' + ', '.join(e.template for e in self) + ')'
 
     @classmethod
-    def load_from_raw(cls, raw: list):
-        l2 = []
-        for c_raw in raw:
-            c = Configuration.load_from_raw(raw=c_raw)
-            l2.append(c)
-        o = Configurations(*l2)
+    def instantiate_from_list(cls, l: list):
+        typed_l = []
+        for d in l:
+            c = Configuration.instantiate_from_dict(d=d)
+            typed_l.append(c)
+        o = Configurations(*typed_l)
         return o
 
     def to_yaml(self, default_flow_style):
@@ -197,12 +251,12 @@ class Representations(tuple):
         return '(' + ', '.join(e.slug for e in self) + ')'
 
     @classmethod
-    def load_from_raw(cls, raw: list):
-        l2 = []
-        for c_raw in raw:
-            c = Representation.load_from_raw(raw=c_raw)
-            l2.append(c)
-        o = Representations(*l2)
+    def instantiate_from_list(cls, l: list):
+        typed_l = []
+        for d in l:
+            o = Representation.instantiate_from_dict(d=d)
+            typed_l.append(o)
+        o = Representations(*typed_l)
         return o
 
     def to_yaml(self, default_flow_style):
@@ -213,7 +267,8 @@ class Representation:
     _in_memory = {}
 
     def __hash__(self):
-        return hash((self.uuid4, self.syntactic_rules, self.configurations))
+        # hash only spans the properties that uniquely identify the object.
+        return hash((self.__class__, self.uuid4))
 
     def __init__(self, uuid4=None, slug=None, syntactic_rules=None, configurations=None):
         self.uuid4 = uuid4
@@ -228,19 +283,22 @@ class Representation:
         return self.slug
 
     @classmethod
-    def load_from_raw(cls, raw: dict, reload: bool = False):
-        uuid4 = raw['uuid4']
+    def instantiate_from_dict(cls, d: dict | None, reload: bool = False):
+        if d is None:
+            d = {}
+        uuid4 = d['uuid4']
         if uuid4 in cls._in_memory.keys() and not reload:
-            logger.debug(f'representation {uuid4} skipped because it was already loaded.')
-            return cls._in_memory[uuid4]
+            o = cls._in_memory[uuid4]
+            logger.debug(f'Reuse {str(o)}({uuid4}).')
+            return o
         else:
             o = Representation()
             o.uuid4 = uuid4
-            o.slug = raw['slug']
-            o.syntactic_rules = SyntacticRules.load_from_raw(
-                raw=raw['syntactic_rules'] if 'syntactic_rules' in raw else None)
-            o.configurations = Configurations.load_from_raw(
-                raw=raw['configurations'] if 'configurations' in raw else None)
+            o.slug = d['slug']
+            o.syntactic_rules = SyntacticRules.instantiate_from_list(
+                d=d['syntactic_rules'] if 'syntactic_rules' in d else None)
+            o.configurations = Configurations.instantiate_from_list(
+                l=d['configurations'] if 'configurations' in d else None)
             cls._in_memory[uuid4] = o
             return o
 
@@ -263,13 +321,12 @@ class Connectors(tuple):
         return '(' + ', '.join(e.slug for e in self) + ')'
 
     @classmethod
-    def load_from_raw(cls, raw: list):
-        l2 = []
-        for c_raw in raw:
-            c = Connector.load_from_raw(raw=c_raw)
-            l2.append(c)
-        o = Connectors(*l2)
-        return o
+    def instantiate_from_list(cls, l: list):
+        typed_l = []
+        for d in l:
+            o = Connector.instantiate_from_dict(d=d)
+            typed_l.append(o)
+        return Connectors(*typed_l)
 
     def to_yaml(self, default_flow_style):
         return yaml.dump(self, default_flow_style=default_flow_style)
@@ -278,6 +335,10 @@ class Connectors(tuple):
 class Connector:
     __slots__ = ('_uuid4', '_slug', '_syntactic_rules', '_representation')
     _uuid4_index = {}
+
+    def __hash__(self):
+        # hash only spans the properties that uniquely identify the object.
+        return hash((self.__class__, self._uuid4))
 
     def __init__(self, uuid4=None, slug=None, syntactic_rules=None, representation=None):
         self._uuid4 = uuid4
@@ -292,15 +353,17 @@ class Connector:
         return self.slug
 
     @classmethod
-    def load_from_raw(cls, raw: dict, reload: bool = False):
-        uuid4 = raw['uuid4']
+    def instantiate_from_dict(cls, d: dict | None, reload: bool = False):
+        if d is None:
+            d = {}
+        uuid4 = d['uuid4']
         if uuid4 in cls._uuid4_index.keys() and not reload:
             logger.debug(f'element {uuid4} skipped because it was already loaded.')
             return cls._uuid4_index[uuid4]
         else:
-            slug = raw['slug']
-            syntactic_rules = SyntacticRules.load_from_raw(
-                raw=raw['syntactic_rules'] if 'syntactic_rules' in raw.keys() else None)
+            slug = d['slug']
+            syntactic_rules = SyntacticRules.instantiate_from_list(
+                d=d['syntactic_rules'] if 'syntactic_rules' in d.keys() else None)
             representation = None
             o = Connector(uuid4=uuid4, slug=slug, syntactic_rules=syntactic_rules, representation=representation)
             cls._uuid4_index[uuid4] = o
@@ -353,15 +416,14 @@ class Theorems(tuple):
         return '(' + ', '.join(e.slug for e in self) + ')'
 
     @classmethod
-    def load_from_raw(cls, raw: list | None):
-        if raw is None:
-            raw = []
-        l2 = []
-        for c_raw in raw:
-            c = Theorem.load_from_raw(raw=c_raw)
-            l2.append(c)
-        o = Connectors(*l2)
-        return o
+    def instantiate_from_list(cls, l: list | None):
+        if l is None:
+            l = []
+        typed_l = []
+        for d in l:
+            o = Theorem.instanciate_from_dict(d=d)
+            typed_l.append(o)
+        return Connectors(*typed_l)
 
     def to_yaml(self, default_flow_style):
         return yaml.dump(self, default_flow_style=default_flow_style)
@@ -382,15 +444,14 @@ class Justifications(tuple):
         return '(' + ', '.join(e.slug for e in self) + ')'
 
     @classmethod
-    def load_from_raw(cls, raw: list | None):
-        if raw is None:
-            raw = []
-        l2 = []
-        for c_raw in raw:
-            c = Justification.load_from_raw(raw=c_raw)
-            l2.append(c)
-        o = Connectors(*l2)
-        return o
+    def instantiate_from_list(cls, l: list | None):
+        if l is None:
+            l = []
+        typed_l = []
+        for d in l:
+            o = Justification.instantiate_from_list(l=d)
+            typed_l.append(o)
+        return Connectors(*typed_l)
 
     def to_yaml(self, default_flow_style):
         return yaml.dump(self, default_flow_style=default_flow_style)
@@ -400,6 +461,10 @@ class Package:
     __slots__ = ('_schema', '_uuid4', '_slug', '_imports', '_aliases', '_representations', '_connectors', '_theorems',
                  '_justifications')
     _uuid4_index = {}
+
+    def __hash__(self):
+        # hash only spans the properties that uniquely identify the object.
+        return hash((self.__class__, self._uuid4))
 
     def __init__(self, schema=None, uuid4=None, slug=None, imports=None, aliases=None, representations=None,
                  connectors=None, theorems=None, justifications=None):
@@ -440,30 +505,29 @@ class Package:
         return self._justifications
 
     @classmethod
-    def load_from_raw(cls, raw: dict, reload: bool = False):
-        uuid4 = raw['uuid4']
+    def instantiate_from_dict(cls, d: dict | None, reload: bool = False):
+        if d is None:
+            d = {}
+        uuid4 = d['uuid4']
         if uuid4 in cls._uuid4_index.keys() and not reload:
             reloaded = cls._uuid4_index[uuid4]
             logger.debug(f'package {reloaded}({uuid4}) skipped because it was already loaded.')
             return reloaded
         else:
-            schema = raw['schema']
-            uuid4 = raw['uuid4']
-            slug = raw['slug']
-            i_list = []
-            for i_as_dict in raw['imports']:
-                # i = Import.load_from_raw(raw=i_as_dict)
-                i_list.append(i_as_dict)
-            imports = i_list
-            aliases = raw['aliases']
-            representations = Representations.load_from_raw(
-                raw=raw['representations'] if 'representations' in raw.keys() else None)
-            connectors = Connectors.load_from_raw(
-                raw=raw['connectors'] if 'connectors' in raw.keys() else None)
-            theorems = Theorems.load_from_raw(
-                raw=raw['theorems'] if 'theorems' in raw.keys() else None)
-            justifications = Justifications.load_from_raw(
-                raw=raw['justifications'] if 'justifications' in raw.keys() else None)
+            schema = d['schema']
+            uuid4 = d['uuid4']
+            slug = d['slug']
+            imports = Imports.instantiate_from_list(
+                l=d['imports'] if 'imports' in d.keys() else None)
+            aliases = d['aliases']
+            representations = Representations.instantiate_from_list(
+                l=d['representations'] if 'representations' in d.keys() else None)
+            connectors = Connectors.instantiate_from_list(
+                l=d['connectors'] if 'connectors' in d.keys() else None)
+            theorems = Theorems.instantiate_from_list(
+                l=d['theorems'] if 'theorems' in d.keys() else None)
+            justifications = Justifications.instantiate_from_list(
+                l=d['justifications'] if 'justifications' in d.keys() else None)
             o = Package(schema=schema, uuid4=uuid4, slug=slug, imports=imports, aliases=aliases,
                         representations=representations, connectors=connectors, theorems=theorems,
                         justifications=justifications)
@@ -473,7 +537,7 @@ class Package:
     def load_from_yaml(cls, yaml_file_path: pathlib.Path):
         global logger
         d = load_yaml_file_path(yaml_file_path=yaml_file_path)
-        o = cls.load_from_raw(raw=d)
+        o = cls.instantiate_from_dict(d=d)
         logger.info(f'package {yaml_file_path} loaded.')
         return o
 
