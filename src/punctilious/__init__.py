8e962bd03c7011f8351cb2e4f2e09a7c622f4395
daviddoret <david.doret@me.com>
2024-12-08 22:30:58 +0100

    identifiers

--- src/punctilious/__init__.py
+++ src/punctilious/__init__.py
@@ -21,25 +21,9 @@ prefs = _presentation.TagsPreferences()
 packages = _foundations.get_packages()
 
 greek_alphabet_lowercase_serif_italic = _greek_alphabet_lowercase_serif_italic.GreekAlphabetLowercaseSerifItalic()
-# print(greek_alphabet_lowercase_serif_italic.alpha.rep(prefs=prefs))
-# print(greek_alphabet_lowercase_serif_italic.phi.configurations[1])
-# print(greek_alphabet_lowercase_serif_italic.psi.configurations[2])
-
 greek_alphabet_uppercase_serif_italic = _greek_alphabet_uppercase_serif_italic.GreekAlphabetUppercaseSerifItalic()
-# print(greek_alphabet_uppercase_serif_italic.psi.rep())
-# print(greek_alphabet_uppercase_serif_italic.phi.configurations[1])
-# print(greek_alphabet_uppercase_serif_italic.psi.configurations[2])
-
-# latin_alphabet_lowercase_serif_italic = _latin_alphabet_lowercase_serif_italic.LatinAlphabetLowercaseSerifItalic()
-# latin_alphabet_lowercase_serif_roman = _latin_alphabet_lowercase_serif_roman.LatinAlphabetLowercaseSerifRoman()
-
 latin_alphabet_uppercase_serif_italic = _latin_alphabet_uppercase_serif_italic.LatinAlphabetUppercaseSerifItalic()
-#
-
 # operators_1 = _operators1.Operators1()
-# print(operators_1.conjunction.representation.configurations[0])
-# print(operators_1.entailment.representation.configurations[1])
-# print(operators_1.entailment.representation.configurations[2])
 
 # pl1 = _propositional_logic1.PropositionalLogic1()
 
