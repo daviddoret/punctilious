a3178ac595d54de1fd72e4dcc51e5f6a5fa9ae2c
daviddoret <david.doret@me.com>
2024-12-14 09:07:47 +0100

    formula-notations

--- tests/test_formula_notations.py
+++ tests/test_formula_notations.py
@@ -3,11 +3,21 @@ import punctilious as pu
 
 
 class TestFormulaNotations:
-    def test_formula_notations(self):
-        connector = 'f'
-        argument = ('x', 'y',)
-        variables = {'connector': connector, 'argument': argument}
-        assert pu.formula_notations.infix_formula.rep(variables=variables) == 'x f y'
-        assert pu.formula_notations.function_formula.rep(variables=variables) == 'f(x, y)'
-        assert pu.formula_notations.prefix_formula.rep(variables=variables) == 'f x'
+    def test_infix_notation(self):
+        assert pu.formula_notations.infix_formula.rep(
+            variables={'connector': 'f', 'argument': ('x', 'y',)}) == 'x f y'
+
+    def test_formula_notation(self):
+        assert pu.formula_notations.function_formula.rep(
+            variables={'connector': 'f', 'argument': ()}) == 'f()'
+        assert pu.formula_notations.function_formula.rep(
+            variables={'connector': 'f', 'argument': ('x',)}) == 'f(x)'
+        assert pu.formula_notations.function_formula.rep(
+            variables={'connector': 'f', 'argument': ('x', 'y',)}) == 'f(x, y)'
+        assert pu.formula_notations.function_formula.rep(
+            variables={'connector': 'f', 'argument': ('x', 'y', 'z',)}) == 'f(x, y, z)'
+
+    def test_prefix_notation(self):
+        assert pu.formula_notations.prefix_formula.rep(
+            variables={'connector': 'f', 'argument': ('x',)}) == 'f x'
         pass
