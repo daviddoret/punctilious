f7d35f7175bf419721fc5933c44a68a94cee3d48
daviddoret <david.doret@me.com>
2024-12-20 21:30:45 +0100

    first theorem implementation

--- tests/test_shared_library.py
+++ tests/test_shared_library.py
@@ -5,7 +5,8 @@ import punctilious as pu
 def create_atomic_connector(c: str):
     ren = pu.RendererForStringConstant(string_constant=c)
     rep = pu.Representation(uid=pu.create_uid(f'rep'), renderers=(ren,))
-    con = pu.Connector(uid=pu.create_uid(f'con'), connector_representation=rep)
+    con = pu.Connector(uid=pu.create_uid(f'con'), connector_representation=rep,
+                       formula_representation=pu.formula_notations.atomic_formula)
     return con
 
 
@@ -15,5 +16,6 @@ def create_function(c: str):
                        '{% endfor %})')
     ren = pu.RendererForStringTemplate(string_template=string_template)
     rep = pu.Representation(uid=pu.create_uid(f'rep'), renderers=(ren,))
-    con = pu.Connector(uid=pu.create_uid(f'fun'), connector_representation=rep)
+    con = pu.Connector(uid=pu.create_uid(f'fun'), connector_representation=rep,
+                       formula_representation=pu.formula_notations.function_formula)
     return con
