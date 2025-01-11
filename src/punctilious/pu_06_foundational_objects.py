b675b2ed0e266ceb4d318d1309cf321e5ee9c09b
daviddoret <david.doret@me.com>
2025-01-11 12:54:35 +0100

    meta-language

--- src/punctilious/pu_06_foundational_objects.py
+++ src/punctilious/pu_06_foundational_objects.py
@@ -383,10 +383,17 @@ class InferenceRule1(_formal_language.Formula):
                 conclusion: _formal_language.Formula):
         return super().__new__(cls, c=_foundational_connectors.inference_rule_1, a=(variables, premises, conclusion,))
 
-    def _check_arguments_validity(self, arguments: ExtensionTuple) -> [bool, ExtensionTuple | None,
-                                                                       ExtensionMap | None]:
+    def _check_arguments_validity(self, arguments: ExtensionTuple, raise_error_if_false: bool = False) -> [bool,
+                                                                                                           ExtensionTuple | None,
+                                                                                                           ExtensionMap | None]:
         """Internal function. Factors validation code between `apply_rule` and `check_arguments_validity`."""
         if arguments.arity != self.premises.arity:
+            if raise_error_if_false:
+                raise ValueError(
+                    f'The arity of the `arguments` ({arguments.arity})'
+                    f' is not equal to the arity of the `premises` ({self.premises.arity}).'
+                    f' `arguments`: `{arguments}`.'
+                    f' `premises`: {self.premises}.')
             return False, None, None
 
         # the arguments are passed as an ExtensionTuple.
@@ -403,11 +410,21 @@ class InferenceRule1(_formal_language.Formula):
         )
         check: bool
         if not check:
+            if raise_error_if_false:
+                raise ValueError(
+                    f'The `arguments` ({arguments})'
+                    f' are not equal formula-equivalent-with-variables with the  `premises` ({self.premises}).'
+                    f' `variables`: `{self.variables}`.')
             return False, None, None
 
         # confirm that all variables have been assigned a value.
         for variable in self.variables.elements:
             if variable not in m.domain.elements:
+                if raise_error_if_false:
+                    raise ValueError(
+                        f'The inference of variable values from the `arguments` ({arguments}) failed.'
+                        f' Some variables are left without values `m` ({m}).'
+                        f' `premises`: `{self.premises}`.')
                 return False, None, None
 
         return True, premises_as_extension_tuple, m
@@ -425,33 +442,8 @@ class InferenceRule1(_formal_language.Formula):
         return self.arguments[0]
 
     def apply_rule(self, arguments: ExtensionTuple) -> _formal_language.Formula:
-        if arguments.arity != self.premises.arity:
-            raise ValueError('arities dont match')
-
-        # the arguments are passed as an ExtensionTuple.
-        # in order to check formula-equivalence-with-variables between
-        # arguments and premises, we must convert premises to ExtensionTuple.
-        premises_as_extension_tuple: ExtensionTuple = self.premises.to_extension_tuple()
-
-        # the arguments must be formula-equivalent-with variables to the premises.
-        # simultaneously, infer the variable values.
-        check, m = is_formula_equivalent_with_variables(
-            formula_without_variables=arguments,
-            variables=self.variables,
-            formula_with_variables=premises_as_extension_tuple
-        )
-        check: bool
-        m: ExtensionMap
-        if not check:
-            raise ValueError(
-                f'The `arguments` ({arguments}) are not formula-equivalent-with-variables'
-                f' with the `premises` ({self.premises})'
-                f' considering `variables` ({self.variables}).')
-
-        # confirm that all variables have been assigned a value.
-        for variable in self.variables.elements:
-            if variable not in m.domain.elements:
-                raise ValueError(f'`variable` ({variable}) is not assigned a value.')
+        check, premises_as_extension_tuple, m = self._check_arguments_validity(arguments=arguments,
+                                                                               raise_error_if_false=True)
 
         # transform the conclusion.
         conclusion_with_variable_assignments: _formal_language.Formula = substitute_formulas(
