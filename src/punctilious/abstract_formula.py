5d18d0ed2cffd7f479348eb3b1a34efbf076426e
dm3d <809400+daviddoret@users.noreply.github.com>
2025-06-08 11:53:23 +0200

    AbstractFormula

--- src/punctilious/abstract_formula.py
+++ src/punctilious/abstract_formula.py
@@ -1,12 +1,80 @@
 from __future__ import annotations
 import typing
 import collections
+import enum
 
 # package modules
 import util
 import rooted_plane_tree as rpt
 import restricted_growth_function as rgf
+from punctilious.restricted_growth_function import RestrictedGrowthFunctionSequence
 from punctilious.rooted_plane_tree import RootedPlaneTree
+
+
+class FormulaIterationNavigator:
+    def __init__(self):
+        self.is_start = True
+        self.is_move_down = False
+        self.is_move_right = False
+        self.is_move_up = False
+        self.is_end = False
+        self.rooted_plane_tree_depth = 1
+        self.rooted_plane_tree_children_index = 1
+        self.restricted_growth_function_sequence_index = 1
+
+    def __str__(self):
+        if self.is_start:
+            return "S"
+        elif self.is_move_down:
+            return "D"
+        elif self.is_move_up:
+            return "U"
+        elif self.is_move_right:
+            return "R"
+        elif self.is_end:
+            return "E"
+        else:
+            raise util.PunctiliousException('ooops')
+
+    def move_down(self):
+        self.is_start = False
+        self.is_move_down = True
+        self.is_move_right = False
+        self.is_move_up = False
+        self.is_end = False
+        self.rooted_plane_tree_children_index = None
+        self.rooted_plane_tree_depth += 1
+
+    def move_right(self, i: int):
+        """
+
+        :param i: The rooted_plane_tree_children_index.
+        :return:
+        """
+        self.is_start = False
+        self.is_move_down = False
+        self.is_move_right = True
+        self.is_move_up = False
+        self.is_end = False
+        self.rooted_plane_tree_children_index = i
+        self.restricted_growth_function_sequence_index += 1
+
+    def move_up(self):
+        self.is_start = False
+        self.is_move_down = False
+        self.is_move_right = False
+        self.is_move_up = True
+        self.is_end = False
+        self.rooted_plane_tree_children_index = None
+        self.rooted_plane_tree_depth -= 1
+
+    def end(self):
+        self.is_start = False
+        self.is_move_down = False
+        self.is_move_right = False
+        self.is_move_up = False
+        self.is_end = True
+        self.rooted_plane_tree_children_index = None
 
 
 def data_validate_abstract_formula(
@@ -84,12 +152,35 @@ class AbstractFormula(tuple):
         """A shortcut for self.rooted_plane_tree."""
         return self.rooted_plane_tree
 
+    def iterate_formula_components_depth_first_ascending(self):
+        nav = FormulaIterationNavigator()
+        yield nav
+        if self.rooted_plane_tree.degree > 0:
+            nav.move_down()
+            yield nav
+            for i in range(0, self.rooted_plane_tree.degree):
+                nav.move_right(i)
+                yield nav
+            nav.move_up()
+            yield nav
+        nav.end()
+        yield nav
+
     def to_default_representation(self) -> str:
         """"""
-        TODO: RESUME
-        HERE
-        output = f"{str(self.restricted_growth_function_sequence[0])}("
-        output +=
+        output = ""
+        for nav in self.iterate_formula_components_depth_first_ascending():
+            if nav.is_start:
+                output += str(self.restricted_growth_function_sequence[nav.restricted_growth_function_sequence_index])
+            elif nav.is_move_down:
+                output += "("
+            elif nav.is_move_right:
+                if nav.rooted_plane_tree_children_index > 1:
+                    output += ", "
+                output += str(self.restricted_growth_function_sequence[nav.restricted_growth_function_sequence_index])
+            elif nav.is_move_up:
+                output += ")"
+        return output
 
 
 FlexibleAbstractFormula = typing.Union[
