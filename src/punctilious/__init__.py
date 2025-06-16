f2f8d71ff70bcde557f04812812dd21a0c2f4fb4
dm3d <809400+daviddoret@users.noreply.github.com>
2025-06-16 20:49:30 +0200

    __util__ cleanup

--- src/punctilious/__init__.py
+++ src/punctilious/__init__.py
@@ -1,8 +1,18 @@
 import util as util
-import rooted_plane_tree as rpt
-import restricted_growth_function as rgf
-import abstract_formula as af
+import rooted_plane_tree
+import restricted_growth_function
+import abstract_formula
 import connective
-import connective_sequence as cs
+import connective_sequence
 import connective_library
 import formula
+
+af = abstract_formula
+cl = connective_library
+cs = connective_sequence
+rgf = restricted_growth_function
+rpt = rooted_plane_tree
+
+__all__ = ['abstract_formula', 'af', 'cl', 'connective', 'connective_library', 'connective_sequence', 'cs',
+           'restricted_growth_function', 'rgf',
+           'rooted_plane_tree', 'rpt']
