3aeab746c3b81858a6441b856ff8e91a3b7b9cfb
dm3d <809400+daviddoret@users.noreply.github.com>
2025-07-06 09:13:21 +0200

    abstract-map

--- src/punctilious/__init__.py
+++ src/punctilious/__init__.py
@@ -4,23 +4,21 @@ import connective_library
 import connective_catalog
 import sequence_library
 import abstract_formula_library
-import abstract_structure_library
+import formula_library
 
 # import formula_library
 
 afl = abstract_formula_library
-asl = abstract_structure_library
 cl = connective_library
 cc = connective_catalog
-# fl = formula_library
+fl = formula_library
 rptl = rooted_plane_tree_library
 sl = sequence_library
 
 __all__ = [
     "abstract_formula_library", "afl",
-    "abstract_structure_library", "asl",
     "connective_library", "cl",
     "connective_catalog", "cc",
-    #   "formula_library", "fl",
+    "formula_library", "fl",
     "rooted_plane_tree_library", "rptl",
     "sequence_library", "sl"]
