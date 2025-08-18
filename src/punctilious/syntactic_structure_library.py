2b0937861b6c39e16d4ce3312021129e40538d7b
dm3d <809400+daviddoret@users.noreply.github.com>
2025-08-18 09:32:17 +0200

    AbstractOrderedSet

--- src/punctilious/syntactic_structure_library.py
+++ src/punctilious/syntactic_structure_library.py
@@ -9,6 +9,7 @@ import functools
 
 import labeled_rooted_plane_tree_library as lrptl
 from punctilious.labeled_rooted_plane_tree_library import LabeledRootedPlaneTree
+from punctilious.transformation_library import AbstractOrderedSet
 
 
 class SyntacticStructure:
@@ -58,6 +59,100 @@ class SyntacticStructure:
         return self._labeled_rooted_plane_tree
 
 
+class AbstractOrderedSet(SyntacticStructure):
+    r"""An abstract ordered set.
+
+    Definition
+    ------------
+
+    An `abstract ordered set` is a syntactic structure that models a finite (computable) ordered set defined by extension.
+
+    Given LRPT :math:`T` with sub-LRPTs :math:`t_0, t_1, \cdots, t_n`,
+    its `abstract ordered set` is the ordered set :math:`(s_0, s_1, \cdots, s_m)`
+    where :math:`s_i` denotes the :math:`i`-th *unique* immediate sub-LRPTs of :math:`T`, preserving order.
+
+    Sample
+    --------
+    Given :math:`T = 0(3, 1, 1, 1, 2)`, the corresponding abstract ordered set is :math:`(3, 1, 2)`.
+
+    Note
+    ------
+
+    The main element of the LRPT is not an information of the abstract ordered set, i.e.: it is dropped.
+
+    Note
+    -----------
+
+    Every LRPT is an abstract ordered set.
+
+    Note
+    ----------
+
+    Every LRPT of degree 0 is the empty ordered set.
+
+    """
+
+    def __init__(self, lrpt: lrptl.FlexibleLabeledRootedPlaneTree):
+        super().__init__(lrpt)
+
+    @functools.cached_property
+    def cardinality(self) -> int:
+        r"""Returns the cardinality of this abstract ordered set.
+
+        Note
+        ______
+
+        This is equal to the number of unique immediate sub-LRPTs
+        in the LRPT of this syntactic structure.
+
+        This is equivalent to the degree of the LRPT
+        if its immediate sub-LRPTs are unique.
+
+        :return: an integer.
+        """
+        return len(self.elements)
+
+    @functools.cached_property
+    def elements(self) -> tuple[lrptl.LabeledRootedPlaneTree, ...]:
+        r"""Returns the elements of this abstract ordered set.
+
+        Note
+        ______
+
+        This is equivalent to the unique and immediate subtrees of the LRPT, preserving order.
+
+        :return: The elements of the ordered set, in order.
+        """
+        unique_elements: tuple[lrptl.LabeledRootedPlaneTree, ...] = ()
+        for sub_lrpt in self.labeled_rooted_plane_tree.iterate_immediate_subtrees():
+            if sub_lrpt not in unique_elements:
+                unique_elements = unique_elements + (sub_lrpt,)
+        return unique_elements
+
+    def has_element(self, x: lrptl.FlexibleLabeledRootedPlaneTree) -> bool:
+        r"""Returns `True` if `x` is an element of this set, `False` otherwise.
+
+        :param x: An object.
+        :return: `True` or `False`
+        """
+        return self._labeled_rooted_plane_tree.has_immediate_subtree(x)
+
+    def is_abstract_ordered_set_equivalent_to(self, x: FlexibleSyntacticStructure) -> bool:
+        r"""Check if this syntactic structure is abstract ordered set equivalent to `x`.
+
+        Definition:
+
+        :param x: A syntactic structure.
+        :return: `True` or `False`.
+        """
+
+        # direct conversion to abstract set is possible because abstract ordered sets have no constraints.
+        x: AbstractOrderedSet = AbstractOrderedSet.from_any(x)
+
+        # direct comparison of elements is possible because order is considered.
+        return self.elements == x.elements
+
+
 class AbstractSet(SyntacticStructure):
     r"""An abstract set.
 
@@ -74,6 +169,8 @@ class AbstractSet(SyntacticStructure):
     ------
 
     The main element of the LRPT is not an information of the abstract set, i.e.: it is dropped.
+
+    The order of immediate sub-LRPTs in the LRPT is not an information of the abstract set, i.e.: it is dropped.
 
     Note
     -----------
@@ -120,7 +217,7 @@ class AbstractSet(SyntacticStructure):
         ------
 
         Even though sets are not ordered, by convention the elements of an abstract set
-        are ordered by LRPT canonical order.
+        are ordered by LRPT canonical order, facilitating equivalence checking.
 
         :return: The elements of the set.
         """
@@ -163,4 +260,6 @@ FlexibleSyntacticStructure = typing.Union[
 
 # Aliases
 
-SR = SyntacticStructure  # An alias
+AOS = AbstractOrderedSet  # An alias for :class:`AbstractOrderedSet`.
+AS = AbstractSet  # An alias for :class:`AbstractSet`.
+SR = SyntacticStructure  # An alias for :class:`SyntacticStructure`.
--- tests/test_syntactic_structure.py
+++ tests/test_syntactic_structure.py
@@ -1,6 +1,66 @@
 import punctilious as pu
 
 
+class TestAbstractOrderedSet:
+
+    def test_abstract_ordered_set(self):
+        # declare ordered set {1, 2, 3}
+        t0_123 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (1, 2, 3,)))
+        t0 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(0)
+        t1 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(1)
+        t2 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(2)
+        t3 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(3)
+        t4 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(4)
+        s123 = pu.ssl.AbstractOrderedSet(t0_123)
+        assert s123.cardinality == 3
+        assert not s123.has_element(t0_123)
+        assert not s123.has_element(t0)
+        assert s123.has_element(t1)
+        assert s123.has_element(t2)
+        assert s123.has_element(t3)
+        assert not s123.has_element(t4)
+        assert s123.is_abstract_ordered_set_equivalent_to(s123)
+
+        # confirm order matters
+        t0_321 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (3, 2, 1,)))
+        s321 = pu.ssl.AbstractOrderedSet(t0_321)
+        assert s321.cardinality == 3
+        assert not s321.has_element(t0_123)
+        assert not s321.has_element(t0)
+        assert s321.has_element(t1)
+        assert s321.has_element(t2)
+        assert s321.has_element(t3)
+        assert not s321.has_element(t4)
+        assert s321.is_abstract_ordered_set_equivalent_to(s321)
+
+        assert not s123.is_abstract_ordered_set_equivalent_to(s321)
+
+        # confirm duplicates do not matter
+        t0_333221212 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(
+            (0, (3, 3, 3, 2, 2, 1, 2, 1, 2,)))
+        s333221212 = pu.ssl.AbstractOrderedSet(t0_333221212)
+        assert s333221212.cardinality == 3
+        assert not s333221212.has_element(t0_123)
+        assert not s333221212.has_element(t0)
+        assert s333221212.has_element(t1)
+        assert s333221212.has_element(t2)
+        assert s333221212.has_element(t3)
+        assert not s333221212.has_element(t4)
+        assert s333221212.is_abstract_ordered_set_equivalent_to(s333221212)
+
+        assert not s123.is_abstract_ordered_set_equivalent_to(s333221212)
+        assert s321.is_abstract_ordered_set_equivalent_to(s333221212)
+
+        t0_12 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (1, 2,)))
+        s12 = pu.ssl.AbstractOrderedSet(t0_12)
+        assert not s123.is_abstract_ordered_set_equivalent_to(s12)
+        assert s12.cardinality == 2
+
+        s_empty = pu.ssl.AbstractOrderedSet(t0)
+        assert not s123.is_abstract_ordered_set_equivalent_to(s_empty)
+        assert s_empty.cardinality == 0
+
+
 class TestAbstractSet:
 
     def test_abstract_set(self):
