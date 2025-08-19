66c0510e9753a963e56552f25dee033cf6658292
dm3d <809400+daviddoret@users.noreply.github.com>
2025-08-19 08:40:41 +0200

    SyntacticStructure inherits from LRPT

--- tests/test_syntactic_structure.py
+++ tests/test_syntactic_structure.py
@@ -22,6 +22,7 @@ class TestAbstractOrderedSet:
         t3 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(3)
         t4 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(4)
         s123 = pu.ssl.AbstractOrderedSet.from_elements(t1, t2, t3, )
+        assert s123.is_labeled_rooted_plane_tree_equivalent_to(s123)
         assert s123.cardinality == 3
         assert not s123.has_element(s123)
         assert not s123.has_element(t0)
@@ -34,6 +35,7 @@ class TestAbstractOrderedSet:
         # confirm order matters
         t0_321 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (3, 2, 1,)))
         s321 = pu.ssl.AbstractOrderedSet.from_elements(t3, t2, t1)
+        assert s321.is_labeled_rooted_plane_tree_equivalent_to(t0_321)
         assert s321.cardinality == 3
         assert not s321.has_element(s321)
         assert not s321.has_element(t0)
@@ -48,9 +50,10 @@ class TestAbstractOrderedSet:
         # confirm duplicates do not matter
         t0_333221212 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(
             (0, (3, 3, 3, 2, 2, 1, 2, 1, 2,)))
-        s333221212 = pu.ssl.AbstractOrderedSet.from_lrpt(t0_333221212)
+        s333221212 = pu.ssl.AbstractOrderedSet.from_elements(t3, t3, t3, t2, t2, t1, t2, t1, t2)
+        assert s333221212.is_labeled_rooted_plane_tree_equivalent_to(t0_333221212)
         assert s333221212.cardinality == 3
-        assert not s333221212.has_element(t0_123)
+        assert not s333221212.has_element(s333221212)
         assert not s333221212.has_element(t0)
         assert s333221212.has_element(t1)
         assert s333221212.has_element(t2)
@@ -63,6 +66,7 @@ class TestAbstractOrderedSet:
 
         t0_12 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (1, 2,)))
         s12 = pu.ssl.AbstractOrderedSet.from_lrpt(t0_12)
+        assert s12.is_labeled_rooted_plane_tree_equivalent_to(t0_12)
         assert not s123.is_abstract_ordered_set_equivalent_to(s12)
         assert s12.cardinality == 2
 
@@ -81,9 +85,10 @@ class TestAbstractSet:
         t2 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(2)
         t3 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(3)
         t4 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(4)
-        s123 = pu.ssl.AbstractSet(t0_123)
+        s123 = pu.ssl.AbstractSet.from_elements(t1, t2, t3)
+        assert s123.is_labeled_rooted_plane_tree_equivalent_to(t0_123)
         assert s123.cardinality == 3
-        assert not s123.has_element(t0_123)
+        assert not s123.has_element(s123)
         assert not s123.has_element(t0)
         assert s123.has_element(t1)
         assert s123.has_element(t2)
@@ -93,7 +98,8 @@ class TestAbstractSet:
 
         # confirm order does not matter
         t0_321 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (3, 2, 1,)))
-        s321 = pu.ssl.AbstractSet(t0_321)
+        s321 = pu.ssl.AbstractSet.from_elements(t3, t2, t1)
+        assert s321.is_labeled_rooted_plane_tree_equivalent_to(t0_321)
         assert s321.cardinality == 3
         assert not s321.has_element(t0_123)
         assert not s321.has_element(t0)
@@ -107,9 +113,10 @@ class TestAbstractSet:
 
         # confirm duplicates do not matter
         t0_3331212 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (3, 3, 3, 1, 2, 1, 2,)))
-        s3331212 = pu.ssl.AbstractSet(t0_3331212)
+        s3331212 = pu.ssl.AbstractSet.from_elements(t3, t3, t3, t1, t2, t1, t2)
+        assert s3331212.is_labeled_rooted_plane_tree_equivalent_to(t0_3331212)
         assert s3331212.cardinality == 3
-        assert not s3331212.has_element(t0_123)
+        assert not s3331212.has_element(s3331212)
         assert not s3331212.has_element(t0)
         assert s3331212.has_element(t1)
         assert s3331212.has_element(t2)
@@ -120,11 +127,12 @@ class TestAbstractSet:
         assert s123.is_abstract_set_equivalent_to(s3331212)
 
         t0_12 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (1, 2,)))
-        s12 = pu.ssl.AbstractSet(t0_12)
+        s12 = pu.ssl.AbstractSet.from_elements(t1, t2)
+        assert s12.is_labeled_rooted_plane_tree_equivalent_to(t0_12)
         assert not s123.is_abstract_set_equivalent_to(s12)
         assert s12.cardinality == 2
 
-        s_empty = pu.ssl.AbstractSet(t0)
+        s_empty = pu.ssl.AbstractSet.from_lrpt(t0)
         assert not s123.is_abstract_set_equivalent_to(s_empty)
         assert s_empty.cardinality == 0
 
@@ -139,7 +147,8 @@ class TestAbstractTuple:
         t2 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(2)
         t3 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(3)
         t4 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs(4)
-        s123 = pu.ssl.AbstractTuple(t0_123)
+        s123 = pu.ssl.AbstractTuple.from_elements(t1, t2, t3)
+        assert s123.is_labeled_rooted_plane_tree_equivalent_to(t0_123)
         assert s123.cardinality == 3
         assert not s123.has_element(t0_123)
         assert not s123.has_element(t0)
@@ -151,7 +160,8 @@ class TestAbstractTuple:
 
         # confirm order does matter
         t0_321 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (3, 2, 1,)))
-        s321 = pu.ssl.AbstractTuple(t0_321)
+        s321 = pu.ssl.AbstractTuple.from_elements(t3, t2, t1)
+        assert s321.is_labeled_rooted_plane_tree_equivalent_to(t0_321)
         assert s321.cardinality == 3
         assert not s321.has_element(t0_123)
         assert not s321.has_element(t0)
@@ -165,9 +175,10 @@ class TestAbstractTuple:
 
         # confirm duplicates matter
         t0_3331212 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (3, 3, 3, 1, 2, 1, 2,)))
-        s3331212 = pu.ssl.AbstractTuple(t0_3331212)
+        s3331212 = pu.ssl.AbstractTuple.from_elements(t3, t3, t3, t1, t2, t1, t2)
+        assert s3331212.is_labeled_rooted_plane_tree_equivalent_to(t0_3331212)
         assert s3331212.cardinality == 7
-        assert not s3331212.has_element(t0_123)
+        assert not s3331212.has_element(s3331212)
         assert not s3331212.has_element(t0)
         assert s3331212.has_element(t1)
         assert s3331212.has_element(t2)
@@ -178,11 +189,12 @@ class TestAbstractTuple:
         assert not s321.is_abstract_tuple_equivalent_to(s3331212)
 
         t0_12 = pu.lrptl.LabeledRootedPlaneTree.from_tree_of_integer_tuple_pairs((0, (1, 2,)))
-        s12 = pu.ssl.AbstractTuple(t0_12)
+        s12 = pu.ssl.AbstractTuple.from_elements(t1, t2)
+        assert s12.is_labeled_rooted_plane_tree_equivalent_to(t0_12)
         assert not s123.is_abstract_tuple_equivalent_to(s12)
         assert s12.cardinality == 2
 
-        s_empty = pu.ssl.AbstractTuple(t0)
+        s_empty = pu.ssl.AbstractTuple.from_lrpt(t0)
         assert not s123.is_abstract_tuple_equivalent_to(s_empty)
         assert s_empty.cardinality == 0
 
